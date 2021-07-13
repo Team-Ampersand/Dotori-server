73b7dc0f0534a6b7d0d8abac9a2616976e9817c5
taem1n1 <youman7953@naver.com>
2021-07-13 11:07:53 +0900

    Board Entity

--- src/main/java/com/server/Dotori/model/board/Board.java
+++ src/main/java/com/server/Dotori/model/board/Board.java
@@ -1,4 +1,37 @@
 package com.server.Dotori.model.board;
 
-public class BoardTest {
+import com.server.Dotori.model.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity @Table(name = "Board")
+@Builder @Getter
+@NoArgsConstructor @AllArgsConstructor
+public class Board {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "board_title", nullable = false)
+    private String title;
+
+    @Column(name = "board_content", length = 500, nullable = false)
+    private String content;
+
+    @Column(name = "board_time", nullable = false)
+    private LocalDateTime time;
+
 }
