e6e111d761e90d80fa740e948728c0a28d516526
taem1n1 <youman7953@naver.com>
2021-07-13 13:42:06 +0900

    change @JoinColumn name

--- src/main/java/com/server/Dotori/model/point/Point.java
+++ src/main/java/com/server/Dotori/model/point/Point.java
@@ -1,6 +1,8 @@
 package com.server.Dotori.model.point;
 
+import com.server.Dotori.model.BaseTimeEntity;
 import com.server.Dotori.model.member.Member;
+import com.server.Dotori.model.point.enumType.Kind;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,19 +18,23 @@ import static javax.persistence.GenerationType.*;
 @Table(name = "Point")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Point {
+public class Point extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = IDENTITY)
     @Column(name = "point_id")
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "giver_id", nullable = false)
+    @JoinColumn(name = "member_id", nullable = false, insertable = false, updatable = false)
     private Member giverId;
 
     @ManyToOne
-    @JoinColumn(name = "receiver_id", nullable = false)
+    @JoinColumn(name = "member_id", nullable = false, insertable = false, updatable = false)
     private Member receiverId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "point_kind", nullable = false)
+    private Kind kind;
 
     @Column(name = "point_amount", nullable = false)
     private Long amount;
