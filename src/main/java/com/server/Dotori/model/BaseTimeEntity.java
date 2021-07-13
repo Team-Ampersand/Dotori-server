f924301a2eb5c4a7a2a101107c3d62fed6056d5c
taem1n1 <youman7953@naver.com>
2021-07-13 22:44:31 +0900

    createDate 를 업데이트 되지 않도록 설정 하였습니다.

--- src/main/java/com/server/Dotori/model/BaseTimeEntity.java
+++ src/main/java/com/server/Dotori/model/BaseTimeEntity.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 public class BaseTimeEntity {
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @LastModifiedDate
