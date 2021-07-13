ee60448bd486b9cef16c8d7220cb5f302511a397
taem1n1 <youman7953@naver.com>
2021-07-13 13:46:04 +0900

    생성시간과 수정시간을 자동화 해주는 BaseTimeEntity

--- src/main/java/com/server/Dotori/model/BaseTimeEntity.java
+++ src/main/java/com/server/Dotori/model/BaseTimeEntity.java
@@ -1,2 +1,24 @@
-package com.server.Dotori.model;public class BaseTimeEntity {
+package com.server.Dotori.model;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
 }
