e31353a3a7b89401ff2b68e1411cbd2c75158ddb
qoxogus <a01034367055@gmail.com>
2021-07-14 11:39:32 +0900

    [ADD] Response

--- src/main/java/com/server/Dotori/response/result/ListResult.java
+++ src/main/java/com/server/Dotori/response/result/ListResult.java
@@ -1,2 +1,16 @@
-package com.server.Dotori.response.result;public class ListResult {
+package com.server.Dotori.response.result;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ListResult<T> extends CommonResult{
+    private List<T> list;
+
+    public ListResult(CommonResult commonResult, List<T> list){
+        super(commonResult);
+        this.list = list;
+    }
+
 }
--- src/main/java/com/server/Dotori/response/result/SingleResult.java
+++ src/main/java/com/server/Dotori/response/result/SingleResult.java
@@ -1,2 +1,13 @@
-package com.server.Dotori.response.result;public class SingleResult {
+package com.server.Dotori.response.result;
+
+import lombok.Getter;
+
+@Getter
+public class SingleResult<T> extends CommonResult{
+    private T data;
+
+    public SingleResult(CommonResult commonResult, T data){
+        super(commonResult);
+        this.data = data;
+    }
 }
