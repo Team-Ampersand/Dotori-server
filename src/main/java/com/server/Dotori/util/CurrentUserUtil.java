69c0177e54093005fea201b25423dc60696100ad
노경준 <developerjun0615@gmail.com>
2021-07-14 18:01:53 +0900

    ADD CurrentUserUtil

--- src/main/java/com/server/Dotori/util/CurrentUserUtil.java
+++ src/main/java/com/server/Dotori/util/CurrentUserUtil.java
@@ -1,2 +1,37 @@
-package com.server.Dotori.util;public class CurrentUserUtil {
+package com.server.Dotori.util;
+
+import com.server.Dotori.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Member;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentUserUtil {
+    private final UserRepository userRepository;
+
+    public static String getCurrentUserNickname(){
+        String nickname = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof UserDetails) {
+            nickname = ((UserDetails) principal).getUsername();
+        } else{
+            nickname = principal.toString();
+        }
+        return nickname;
+    }
+
+    public Member getCurrentUser() {
+        String nickname = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof UserDetails) {
+            nickname = ((UserDetails) principal).getUsername();
+        } else{
+            nickname = principal.toString();
+        }
+        return (Member) userRepository.findByNickname(nickname);
+    }
 }
