590524ab358e11d0c57efc3a8f621413945c3781
qoxogus <a01034367055@gmail.com>
2021-07-14 11:39:10 +0900

    [ADD] Custom Exception

--- src/main/java/com/server/Dotori/exception/response/CustomException.java
+++ src/main/java/com/server/Dotori/exception/response/CustomException.java
@@ -1,2 +1,25 @@
-package com.server.Dotori.exception.response;public class CustomException {
+package com.server.Dotori.exception.response;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public CustomException(String message, HttpStatus httpStatus){
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+    
+    // http 상태코드를 반환하는 메서드
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
 }
