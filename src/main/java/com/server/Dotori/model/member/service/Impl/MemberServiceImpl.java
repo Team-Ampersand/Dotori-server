b07bb8eee75654ac4bd54a1efa9c3009fba8fb56
노경준 <developerjun0615@gmail.com>
2021-07-19 08:38:32 +0900

    Update MemberServiceImpl signin 기능 구현

--- src/main/java/com/server/Dotori/model/member/service/Impl/MemberServiceImpl.java
+++ src/main/java/com/server/Dotori/model/member/service/Impl/MemberServiceImpl.java
@@ -3,13 +3,18 @@ package com.server.Dotori.model.member.service.Impl;
 import com.server.Dotori.exception.user.exception.UserNotFoundException;
 import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.member.dto.MemberDto;
+import com.server.Dotori.model.member.dto.MemberLoginDto;
 import com.server.Dotori.model.member.enumType.Role;
 import com.server.Dotori.model.member.repository.MemberRepository;
 import com.server.Dotori.model.member.service.MemberService;
+import com.server.Dotori.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.server.Dotori.model.member.enumType.Role.*;
 
@@ -19,6 +24,7 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Value("${authKey.adminKey}")
     private String adminKey;
@@ -49,5 +55,24 @@ public class MemberServiceImpl implements MemberService {
             return userInfo.getId();
         }
         return 0L;
+    }
+
+    @Override
+    public Map<String,String> signin(MemberLoginDto memberLoginDto) {
+        Member findUser = memberRepository.findByUsername(memberLoginDto.getUsername());
+        if(findUser == null) throw new UserNotFoundException();
+
+        boolean passwordCheck = passwordEncoder.matches(memberLoginDto.getPassword(),findUser.getPassword());
+        if(!passwordCheck) throw new UserNotFoundException();
+
+        Map<String,String> map = new HashMap<>();
+        String accessToken = jwtTokenProvider.createToken(findUser.getUsername(), findUser.getRoles());
+        String refreshToken = jwtTokenProvider.createRefreshToken();
+
+        map.put("username",findUser.getUsername());
+        map.put("accessToken","Bearer " + accessToken);
+        map.put("refreshToken","Bearer " + refreshToken);
+
+        return map;
     }
 }
