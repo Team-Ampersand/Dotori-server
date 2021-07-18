018cf2157183dae145425b168556e625a42a1bfe
노경준 <developerjun0615@gmail.com>
2021-07-19 08:37:36 +0900

    Update MemberController1 signin 기능 구현

--- src/main/java/com/server/Dotori/model/member/controller/MemberController1.java
+++ src/main/java/com/server/Dotori/model/member/controller/MemberController1.java
@@ -2,6 +2,7 @@ package com.server.Dotori.model.member.controller;
 
 import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.member.dto.MemberDto;
+import com.server.Dotori.model.member.dto.MemberLoginDto;
 import com.server.Dotori.model.member.service.MemberService;
 import com.server.Dotori.response.ResponseService;
 import com.server.Dotori.response.result.CommonResult;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/v1/member")
+@RequestMapping("/v1")
 public class MemberController1 {
 
     private final MemberService memberService;
@@ -23,6 +26,12 @@ public class MemberController1 {
     public CommonResult signup(@RequestBody MemberDto memberDto){
         memberService.signup(memberDto);
         return responseService.getSuccessResult();
+    }
+
+    @PostMapping("/signin")
+    public CommonResult signin(@RequestBody MemberLoginDto memberLoginDto){
+        Map<String, String> data = memberService.signin(memberLoginDto);
+        return responseService.getSingleResult(data);
     }
 
 
