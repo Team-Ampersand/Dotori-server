2499516e52b025785bf29bb0ef7264476dc66647
노경준 <developerjun0615@gmail.com>
2021-07-19 08:37:55 +0900

    Create MemberLoginDto.java

---
+++ src/main/java/com/server/Dotori/model/member/dto/MemberLoginDto.java
@@ -0,0 +1,11 @@
+package com.server.Dotori.model.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberLoginDto {
+    private String username;
+    private String password;
+}
