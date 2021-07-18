17c5623677895c6ec11a679cf151a58c72d2cd6f
노경준 <developerjun0615@gmail.com>
2021-07-19 08:38:54 +0900

    Update MemberService signin 기능구현

--- src/main/java/com/server/Dotori/model/member/service/MemberService.java
+++ src/main/java/com/server/Dotori/model/member/service/MemberService.java
@@ -2,7 +2,11 @@ package com.server.Dotori.model.member.service;
 
 import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.member.dto.MemberDto;
+import com.server.Dotori.model.member.dto.MemberLoginDto;
+
+import java.util.Map;
 
 public interface MemberService {
     Long signup(MemberDto memberDto);
+    Map<String,String> signin(MemberLoginDto memberLoginDto);
 }
