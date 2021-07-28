3d6f9e694b6fc555f719b9e745fd85eb6a09cb31
qoxogus <a01034367055@gmail.com>
2021-07-28 12:54:58 +0900

    [ADD] getAllBoard with Pageble

--- src/main/java/com/server/Dotori/model/board/service/BoardServiceImpl.java
+++ src/main/java/com/server/Dotori/model/board/service/BoardServiceImpl.java
@@ -1,12 +1,19 @@
 package com.server.Dotori.model.board.service;
 
 import com.server.Dotori.model.board.Board;
+import com.server.Dotori.model.board.dto.BoardGetDto;
 import com.server.Dotori.model.board.dto.BoardSaveDto;
 import com.server.Dotori.model.board.repository.BoardRepository;
 import com.server.Dotori.model.member.Member;
+import com.server.Dotori.model.member.enumType.Role;
 import com.server.Dotori.util.CurrentUserUtil;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +26,18 @@ public class BoardServiceImpl implements BoardService {
     public Board createBoard(BoardSaveDto boardSaveDto) {
         Member currentUser = currentUserUtil.getCurrentUser();
         return boardRepository.save(boardSaveDto.saveToEntity(currentUser));
+    }
+
+    @Override
+    public Page<BoardGetDto> getAllBoard(Pageable pageable) {
+        Page<Board> boardPage = boardRepository.findAll(pageable);
+
+        return boardPage.map(board -> {
+            ModelMapper mapper = new ModelMapper();
+            BoardGetDto map = mapper.map(board, BoardGetDto.class);
+            map.setRoles(board.getMember().getRoles());
+
+            return map;
+        });
     }
 }
