25154b48908df58b33287604b65a8e0032f60d39
노경준 <developerjun0615@gmail.com>
2021-08-02 23:12:18 +0900

    Update MemberService auth 메소드 삭제

--- src/main/java/com/server/Dotori/model/member/service/MemberService.java
+++ src/main/java/com/server/Dotori/model/member/service/MemberService.java
@@ -1,14 +1,11 @@
 package com.server.Dotori.model.member.service;
 
-import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.member.dto.MemberDto;
 import com.server.Dotori.model.member.dto.MemberLoginDto;
-import com.server.Dotori.model.member.dto.UserEmailDto;
 
 import java.util.Map;
 
 public interface MemberService {
     Long signup(MemberDto memberDto);
     Map<String,String> signin(MemberLoginDto memberLoginDto);
-    String auth(UserEmailDto userEmailDto);
 }
