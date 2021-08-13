676734f5fb4d4142d2fa549722d8e29824c2c1e1
qoxogus <a01034367055@gmail.com>
2021-08-13 19:56:12 +0900

    [ADD] querydsl로 음악신청한 회원 음악신청 가능상태로 update하는 쿼리 작성

---
+++ src/main/java/com/server/Dotori/model/music/repository/MusicRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.server.Dotori.model.music.repository;
+
+import com.server.Dotori.model.member.Member;
+
+import java.util.List;
+
+public interface MusicRepositoryCustom {
+
+    void updateMusicStatusMemberByMember();
+}
