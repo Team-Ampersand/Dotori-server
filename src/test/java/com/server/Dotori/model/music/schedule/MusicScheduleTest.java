d81a1f9c33d3b250ca659e9e83713161a9a24df7
qoxogus <a01034367055@gmail.com>
2021-08-24 12:20:20 +0900

    [ADD] 음악목록 삭제 일정이 제대로 설정되었고 작업이 예약되었는지 테스트

--- src/test/java/com/server/Dotori/model/music/schedule/MusicScheduleTest.java
+++ src/test/java/com/server/Dotori/model/music/schedule/MusicScheduleTest.java
@@ -1,4 +1,35 @@
+package com.server.Dotori.model.music.schedule;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.config.CronTask;
+import org.springframework.scheduling.config.ScheduledTask;
+import org.springframework.scheduling.config.ScheduledTaskHolder;
+
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class MusicScheduleTest {
-  
+
+    @Autowired
+    private ScheduledTaskHolder scheduledTaskHolder;
+
+    @Test
+    public void testYearlyCronTaskScheduled() {
+        Set<ScheduledTask> scheduledTasks = scheduledTaskHolder.getScheduledTasks();
+        scheduledTasks.forEach(scheduledTask -> scheduledTask.getTask().getRunnable().getClass().getDeclaredMethods());
+
+        long count = scheduledTasks.stream()
+                .filter(scheduledTask -> scheduledTask.getTask() instanceof CronTask)
+                .map(scheduledTask -> (CronTask) scheduledTask.getTask())
+                .filter(cronTask -> cronTask.getExpression().equals("0 59 23 ? * SAT") && cronTask.toString().equals("com.server.Dotori.model.music.schedule.MusicSchedule.saturdayMusicDeleteAll"))
+                .count();
+        
+        Assertions.assertThat(count).isEqualTo(1L);
+    }
+
 }
