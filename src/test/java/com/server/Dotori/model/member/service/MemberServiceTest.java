3fea3ac2d53aa0f107d177a13501e4b4d729e799
노경준 <developerjun0615@gmail.com>
2021-08-26 20:28:48 +0900

    UPDATE signup TEST 코드 수정

--- src/test/java/com/server/Dotori/model/member/service/MemberServiceTest.java
+++ src/test/java/com/server/Dotori/model/member/service/MemberServiceTest.java
@@ -3,11 +3,18 @@ package com.server.Dotori.model.member.service;
 import com.server.Dotori.model.member.dto.MemberDto;
 import com.server.Dotori.model.member.enumType.Role;
 import com.server.Dotori.model.member.repository.MemberRepository;
+import com.server.Dotori.util.CurrentUserUtil;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,6 +23,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MemberServiceTest {
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private MemberService memberService;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -28,13 +38,23 @@ public class MemberServiceTest {
                 .stdNum("2206")
                 .password("1234")
                 .email("s20018@gsm.hs.kr")
+                .key("ABC1")
                 .build();
         memberDto.setPassword(passwordEncoder.encode(memberDto.getPassword()));
 
         //when
-        memberRepository.save(memberDto.toEntity(Role.ROLE_ADMIN));
+        memberService.signup(memberDto);
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                memberDto.getUsername(),
+                memberDto.getPassword(),
+                List.of(new SimpleGrantedAuthority(Role.ROLE_ADMIN.name())));
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(token);
+        System.out.println("=================================");
+        System.out.println(context);
 
         //then
-        assertThat(memberDto.toEntity(Role.ROLE_ADMIN).getUsername()).isEqualTo(memberRepository.findByUsername("노경준").getUsername());
+        String currentUserName = CurrentUserUtil.getCurrentUserNickname();
+        assertThat(currentUserName).isEqualTo(memberRepository.findByUsername("노경준").getUsername());
     }
 }
