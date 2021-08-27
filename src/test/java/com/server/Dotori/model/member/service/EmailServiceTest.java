2e2812325e93e8b8dd980a52eee11ec89c1e2050
노경준 <developerjun0615@gmail.com>
2021-08-27 16:03:57 +0900

    ADD Email authKey 테스트 코드 작성

--- src/test/java/com/server/Dotori/model/member/service/EmailServiceTest.java
+++ src/test/java/com/server/Dotori/model/member/service/EmailServiceTest.java
@@ -1,2 +1,30 @@
-package com.server.Dotori.model.member.service;public class EmailServiceTest {
+package com.server.Dotori.model.member.service;
+
+import com.server.Dotori.model.member.dto.EmailDto;
+import com.server.Dotori.model.member.dto.MemberEmailKeyDto;
+import com.server.Dotori.util.redis.RedisUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void authKey(){
+        //given
+        EmailDto emailDto = new EmailDto();
+
+        emailDto.setUserEmail("hippo911911@naver.com");
+
+        //when
+        String result = emailService.authKey(emailDto);
+
+        //then
+        System.out.println("result [" + result +"]");
+    }
+
 }
