e5c714990b2ac9124a7299b2a30382f9ee2aaf3a
taem1n1 <youman7953@naver.com>
2021-08-27 11:18:41 +0900

    MemberSave testcode

--- src/test/java/com/server/Dotori/model/member/MemberTest.java
+++ src/test/java/com/server/Dotori/model/member/MemberTest.java
@@ -1,26 +1,21 @@
 package com.server.Dotori.model.member;
 
-import com.server.Dotori.config.querydsl.QuerydslConfiguration;
-import com.server.Dotori.model.member.dto.MemberDto;
 import com.server.Dotori.model.member.enumType.Music;
 import com.server.Dotori.model.member.enumType.Role;
 import com.server.Dotori.model.member.enumType.SelfStudy;
 import com.server.Dotori.model.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
+@Transactional
 public class MemberTest {
 
     @Autowired
@@ -30,26 +25,24 @@ public class MemberTest {
     @DisplayName("Member가 Save가 되는지 검증")
     void Member가_save된다(){
 
-        // Given
-        Member member = Member.builder()
-                .username("taemin")
-                .stdNum("2406")
-                .password("1234")
-                .email("s20014@gsm.hs.kr")
-                .selfStudy(SelfStudy.CAN)
-                .music(Music.CAN)
-                .roles(Collections.singletonList(Role.ROLE_DEVELOPER))
-                .point(0L)
-                .build();
-
-        // When
-
-        Member saveMember = memberRepository.save(member);
-        System.out.println("======== save =========");
+        // Given // When
+        Member saveMember = memberRepository.save(
+                Member.builder()
+                        .username("taemin")
+                        .stdNum("2406")
+                        .password("1234")
+                        .email("s20014@gsm.hs.kr")
+                        .roles(Collections.singletonList(Role.ROLE_ADMIN))
+                        .music(Music.CAN)
+                        .selfStudy(SelfStudy.CAN)
+                        .point(0L)
+                        .answer("때밀이")
+                        .build()
+        );
 
 
         // Then
-        assertEquals(member, saveMember);
+        Assertions.assertThat(saveMember.getUsername()).isEqualTo("taemin");
 
     }
 
