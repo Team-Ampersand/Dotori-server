055ec6af49ddac8e87019f7845bef1176cab354e
노경준 <developerjun0615@gmail.com>
2021-08-28 17:29:10 +0900

    Update Test 코드 수정

--- src/test/java/com/server/Dotori/model/member/service/EmailServiceTest.java
+++ src/test/java/com/server/Dotori/model/member/service/EmailServiceTest.java
@@ -1,11 +1,17 @@
 package com.server.Dotori.model.member.service;
 
 import com.server.Dotori.model.member.dto.EmailDto;
+import com.server.Dotori.model.member.dto.MemberDto;
 import com.server.Dotori.model.member.dto.MemberEmailKeyDto;
 import com.server.Dotori.util.redis.RedisUtil;
+
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 public class EmailServiceTest {
@@ -13,18 +19,34 @@ public class EmailServiceTest {
     @Autowired
     private EmailService emailService;
 
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     void authKey(){
         //given
-        EmailDto emailDto = new EmailDto();
+        MemberDto memberDto = MemberDto.builder()
+                .username("노경준")
+                .stdNum("2206")
+                .password("1234")
+                .email("s20018@gsm.hs.kr")
+                .key("ABC1")
+                .answer("hello")
+                .build();
+        memberDto.setPassword(passwordEncoder.encode(memberDto.getPassword()));
 
-        emailDto.setUserEmail("hippo911911@naver.com");
+        EmailDto emailDto = new EmailDto();
+        emailDto.setUserEmail(memberDto.getEmail());
 
         //when
-        String result = emailService.authKey(emailDto);
+        String key = emailService.authKey(emailDto);
 
         //then
-        System.out.println("result [" + result +"]");
+        System.out.println("======================== " + key + " =========================");
+        Assertions.assertThat(memberDto.getEmail()).isEqualTo(redisUtil.getData(key));
     }
 
 }
