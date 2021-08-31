bda6d45d5540d20a07f8c1d85996584d9e26c22a
노경준 <developerjun0615@gmail.com>
2021-08-31 09:31:41 +0900

    ADD existsByUsername 메소드 추가

--- src/main/java/com/server/Dotori/model/member/repository/MemberRepository.java
+++ src/main/java/com/server/Dotori/model/member/repository/MemberRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Member findByUsername(String username);
     Member findByEmail(String email);
+    boolean existsByUsername(String username);
 }
