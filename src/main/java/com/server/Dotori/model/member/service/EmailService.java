fec64f7c3d42d644c62c644ab8fada6ae32bb03e
노경준 <developerjun0615@gmail.com>
2021-08-31 09:32:30 +0900

    Update return 값 Member로 변경

--- src/main/java/com/server/Dotori/model/member/service/EmailService.java
+++ src/main/java/com/server/Dotori/model/member/service/EmailService.java
@@ -1,10 +1,11 @@
 package com.server.Dotori.model.member.service;
 
+import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.member.dto.EmailDto;
 import com.server.Dotori.model.member.dto.MemberEmailKeyDto;
 
 public interface EmailService {
     String authKey(EmailDto emailDto);
     String authCheck(MemberEmailKeyDto memberEmailKeyDto);
-    String authPassword(EmailDto emailDto);
+    Member authPassword(EmailDto emailDto);
 }
