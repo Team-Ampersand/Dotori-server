d13581e701f3b5cd3463eb7f3a0fed88224a3c92
qoxogus <a01034367055@gmail.com>
2021-09-01 14:35:30 +0900

    [ADD] service 로직 작성

--- src/main/java/com/server/Dotori/model/member/service/point/PointService.java
+++ src/main/java/com/server/Dotori/model/member/service/point/PointService.java
@@ -1,8 +1,13 @@
 package com.server.Dotori.model.member.service.point;
 
+import com.server.Dotori.model.member.dto.point.GetPointDto;
 import com.server.Dotori.model.member.dto.point.PointDto;
+
+import java.util.List;
 
 public interface PointService {
 
     void point(PointDto pointDto);
+
+    List<GetPointDto> getAllStudentPoint(Long id);
 }
--- src/main/java/com/server/Dotori/model/member/service/point/PointServiceImpl.java
+++ src/main/java/com/server/Dotori/model/member/service/point/PointServiceImpl.java
@@ -2,11 +2,14 @@ package com.server.Dotori.model.member.service.point;
 
 import com.server.Dotori.exception.user.exception.UserNotFoundException;
 import com.server.Dotori.model.member.Member;
+import com.server.Dotori.model.member.dto.point.GetPointDto;
 import com.server.Dotori.model.member.repository.MemberRepository;
 import com.server.Dotori.model.member.dto.point.PointDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +24,14 @@ public class PointServiceImpl implements PointService {
                 .orElseThrow(() -> new UserNotFoundException());
 
         findReceiverMember.updatePoint(findReceiverMember.getPoint() + pointDto.getPoint());
+    }
+
+    @Override
+    public List<GetPointDto> getAllStudentPoint(Long id) {
+        List<GetPointDto> studentPoint = memberRepository.findStudentPoint(id);
+
+        if (studentPoint.isEmpty()) throw new IllegalArgumentException("해당 반에 해당하는 학생이 없습니다.");
+
+        return studentPoint;
     }
 }
