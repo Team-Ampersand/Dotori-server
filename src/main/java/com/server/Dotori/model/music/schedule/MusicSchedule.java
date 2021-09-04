c4a6245a2197d7e7efd6913ea83045f1516db275
qoxogus <a01034367055@gmail.com>
2021-09-04 20:27:00 +0900

    [ADD] 평일 자정에 학생들의 음악신청 상태를 변경해주는 기능 자동화

--- src/main/java/com/server/Dotori/model/music/schedule/MusicSchedule.java
+++ src/main/java/com/server/Dotori/model/music/schedule/MusicSchedule.java
@@ -1,25 +1,28 @@
 package com.server.Dotori.model.music.schedule;
 
 import com.server.Dotori.model.music.repository.MusicRepository;
+import com.server.Dotori.model.music.service.MusicService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class MusicSchedule {
 
     private final MusicRepository musicRepository;
-
-    public MusicSchedule(MusicRepository musicRepository) {
-        this.musicRepository = musicRepository;
-    }
+    private final MusicService musicService;
 
     @Scheduled(cron = "0 59 23 ? * SAT", zone = "GMT+9")
     public void saturdayMusicDeleteAll() {
         musicRepository.deleteAll();
+    }
+
+    @Scheduled(cron = "0 0 0 ? * MON-FRI", zone = "GMT+9")
+    public void weekdayMusicStatusReset() {
+        musicService.updateMemberMusicStatus();
     }
 
     /**
