68066a478123f57541447608969abb60ee128e08
노경준 <developerjun0615@gmail.com>
2021-09-07 18:12:25 +0900

    Update MemberDto key 변수 삭제 및 swagger 기본 value 설정

--- src/main/java/com/server/Dotori/model/member/dto/MemberDto.java
+++ src/main/java/com/server/Dotori/model/member/dto/MemberDto.java
@@ -4,6 +4,7 @@ import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.member.enumType.Music;
 import com.server.Dotori.model.member.enumType.Role;
 import com.server.Dotori.model.member.enumType.SelfStudy;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
@@ -18,32 +19,34 @@ import java.util.Collections;
 public class MemberDto {
     @NotBlank(message = "username should be valid")
     @Size(min = 1, max = 10)
+    @ApiModelProperty(example = "노경준")
     private String username;
 
     @NotBlank(message = "stdNum should be valid")
     @Size(min = 4, max = 4)
+    @ApiModelProperty(example = "2206")
     private String stdNum;
 
     @NotBlank(message = "password should be valid")
     @Size(min = 4)
+    @ApiModelProperty(example = "1234")
     private String password;
 
     @NotBlank(message = "email should be valid")
+    @ApiModelProperty(example = "s20018@gsm.hs.kr")
     private String email;
 
-    @NotBlank(message = "roles should be valid")
-    private String key;
-
     @NotBlank(message = "answer should be valid")
+    @ApiModelProperty(example = "노갱")
     private String answer;
 
-    public Member toEntity(Role roleAdmin){
+    public Member toEntity(){
         return Member.builder()
                 .username(username)
                 .stdNum(stdNum)
                 .password(password)
                 .email(email)
-                .roles(Collections.singletonList(roleAdmin))
+                .roles(Collections.singletonList(Role.ROLE_MEMBER))
                 .music(Music.CAN)
                 .selfStudy(SelfStudy.CAN)
                 .point(0L)
