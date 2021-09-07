9b84934d730b684858f77617dad7e7369f3abea0
qoxogus <a01034367055@gmail.com>
2021-09-07 12:18:51 +0900

    [ADD] Scheduled 주석 추가

--- src/main/java/com/server/Dotori/model/music/schedule/MusicSchedule.java
+++ src/main/java/com/server/Dotori/model/music/schedule/MusicSchedule.java
@@ -15,11 +15,17 @@ public class MusicSchedule {
     private final MusicRepository musicRepository;
     private final MusicService musicService;
 
+    /**
+     * "토요일 23시 59분"에 음악신청 목록을 자동으로 초기화해주는 Scheduled (repository -> service 로 변경할예정)
+     */
     @Scheduled(cron = "0 59 23 ? * SAT", zone = "GMT+9")
     public void saturdayMusicDeleteAll() {
         musicRepository.deleteAll();
     }
 
+    /**
+     * "월 ~ 금 자정"에 음악신청 상태를 자동으로 update 해주는 Scheduled
+     */
     @Scheduled(cron = "0 0 0 ? * MON-FRI", zone = "GMT+9")
     public void weekdayMusicStatusReset() {
         musicService.updateMemberMusicStatus();
