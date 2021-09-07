0b2461a53f995789b12d7ec1eac24c2d3bfac82f
qoxogus <a01034367055@gmail.com>
2021-09-07 13:30:49 +0900

    [ADD] 테스트용 컨트롤러 추가

--- src/test/java/com/server/Dotori/model/music/service/MusicServiceTest.java
+++ src/test/java/com/server/Dotori/model/music/service/MusicServiceTest.java
@@ -9,6 +9,7 @@ import com.server.Dotori.model.member.repository.MemberRepository;
 import com.server.Dotori.model.member.service.MemberService;
 import com.server.Dotori.model.music.Music;
 import com.server.Dotori.model.music.dto.MusicApplicationDto;
+import com.server.Dotori.model.music.dto.MusicResDto;
 import com.server.Dotori.model.music.repository.MusicRepository;
 import com.server.Dotori.util.CurrentUserUtil;
 import org.assertj.core.api.Assertions;
@@ -108,7 +109,7 @@ class MusicServiceTest {
         musicRepository.saveAll(musicList);
 
         //when
-        List<Music> getAllMusic = musicService.getAllMusic();
+        List<MusicResDto> getAllMusic = musicService.getAllMusic();
 
         //then
         assertEquals(getAllMusic.size(), 30);
