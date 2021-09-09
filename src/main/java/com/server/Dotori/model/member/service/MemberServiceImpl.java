99c4400f9f30b9b6889d674f9f79401cc9ede06a
노경준 <developerjun0615@gmail.com>
2021-09-09 17:48:06 +0900

    ADD MemberServiceImpl passwordChange 추가

--- src/main/java/com/server/Dotori/model/member/service/MemberServiceImpl.java
+++ src/main/java/com/server/Dotori/model/member/service/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package com.server.Dotori.model.member.service;
 
 import com.server.Dotori.exception.user.exception.UserAlreadyException;
 import com.server.Dotori.exception.user.exception.UserNotFoundException;
+import com.server.Dotori.exception.user.exception.UserPasswordNotMatchingException;
 import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.member.dto.MemberDto;
 import com.server.Dotori.model.member.dto.MemberLoginDto;
@@ -16,6 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,22 +56,44 @@ public class MemberServiceImpl implements MemberService {
      */
     @Override
     public Map<String,String> signin(MemberLoginDto memberLoginDto) {
-        Member findUser = memberRepository.findByEmail(memberLoginDto.getEmail());
-        if(findUser == null) throw new UserNotFoundException();
+        Member findUser = memberRepository.findByEmail(memberLoginDto.getEmail()); // email로 유저정보를 가져옴
+        if(findUser == null) throw new UserNotFoundException(); // 유저가 존재하는지 체크
 
-        boolean passwordCheck = passwordEncoder.matches(memberLoginDto.getPassword(),findUser.getPassword());
-        if(!passwordCheck) throw new UserNotFoundException();
+        boolean passwordCheck = passwordEncoder.matches(memberLoginDto.getPassword(),findUser.getPassword()); // 비밀번호가 DB에 있는 비밀번호와 입력된 비밀번호가 같은지 체크
+        if(!passwordCheck) throw new UserNotFoundException(); // 비밀번호가 DB에 있는 비밀번호와 입력된 비밀번호가 같은지 체크
 
-        Map<String,String> map = new HashMap<>();
-        String accessToken = jwtTokenProvider.createToken(findUser.getUsername(), findUser.getRoles());
+        Map<String,String> map = new HashMap<>(); // Token 을 담을수있는 Hashmap 선언
+        String accessToken = jwtTokenProvider.createToken(findUser.getUsername(), findUser.getRoles()); // 유저정보에서 Username : 유저정보에서 Role (Key : Value)
         String refreshToken = jwtTokenProvider.createRefreshToken();
 
-        redisUtil.deleteData(memberLoginDto.getEmail());
-        redisUtil.setDataExpire(memberLoginDto.getEmail(),refreshToken,jwtTokenProvider.REFRESH_TOKEN_VALIDATION_SECOND);
+        redisUtil.deleteData(findUser.getUsername()); // redis에 넣기전 유저정보 삭제
+        redisUtil.setDataExpire(findUser.getUsername(),refreshToken,jwtTokenProvider.REFRESH_TOKEN_VALIDATION_SECOND); // redis에 key를 Username Value를 refresh입력
 
-        map.put("username",findUser.getUsername());
+        // map에 email, accessToken, refreshToken 정보 입력
+        map.put("userName", findUser.getUsername());
         map.put("accessToken","Bearer " + accessToken);
         map.put("refreshToken","Bearer " + refreshToken);
+
+        return map;
+    }
+
+    /**
+     * 현재 비밀번호, 새로운 비밀번호를 받아서 비밀번호 변경
+     * @param memberPasswordDto memberPasswordDto
+     * @return
+     */
+    @Transactional
+    @Override
+    public Map<String,String> passwordChange(MemberPasswordDto memberPasswordDto) {
+        Member findMember = currentUserUtil.getCurrentUser();
+        if(!passwordEncoder.matches(memberPasswordDto.getOldPassword(),findMember.getPassword()))
+            throw new UserPasswordNotMatchingException();
+
+        String encodePw = passwordEncoder.encode(memberPasswordDto.getNewPassword());
+        findMember.updatePassword(encodePw);
+
+        Map<String,String> map = new HashMap<>();
+        map.put(findMember.getUsername(),findMember.getPassword());
 
         return map;
     }
