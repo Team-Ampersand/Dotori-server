8cee6be73648fa24caeae363f848fcac6cc415dd
qoxogus <a01034367055@gmail.com>
2021-09-09 13:19:39 +0900

    [UPDATE] 주석 보완

--- src/main/java/com/server/Dotori/model/member/service/mainpage/MainPageServiceImpl.java
+++ src/main/java/com/server/Dotori/model/member/service/mainpage/MainPageServiceImpl.java
@@ -15,7 +15,8 @@ public class MainPageServiceImpl implements MainPageService {
 
     /**
      * 메인페이지를 조회했을 때 프로필정보를 반환해주는 서비스로직 (로그인된 유저 사용가능)
-     * @return GetAboutPointDto id, username, stNum, point
+     * @return GetAboutPointDto (id, username, stNum, point)
+     * @author 배태현
      */
     @Override
     public GetAboutPointDto getMyProfile() {
