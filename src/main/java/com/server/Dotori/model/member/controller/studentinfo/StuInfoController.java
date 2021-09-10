a37cc4222fd4afc592ef65de05ff79b933e08d3f
qoxogus <a01034367055@gmail.com>
2021-09-10 10:51:05 +0900

    [ADD] 테스트용 컨트롤러 추가

--- src/main/java/com/server/Dotori/model/member/controller/studentinfo/StuInfoController.java
+++ src/main/java/com/server/Dotori/model/member/controller/studentinfo/StuInfoController.java
@@ -1,2 +1,33 @@
-package com.server.Dotori.model.member.controller.studentinfo;public class StuInfoController {
+package com.server.Dotori.model.member.controller.studentinfo;
+
+import com.server.Dotori.model.member.dto.StudentInfoDto;
+import com.server.Dotori.model.member.service.studentinfo.StuInfoService;
+import com.server.Dotori.response.ResponseService;
+import com.server.Dotori.response.result.CommonResult;
+import com.server.Dotori.response.result.SingleResult;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/member")
+@RequiredArgsConstructor
+public class StuInfoController {
+
+    private final ResponseService responseService;
+    private final StuInfoService stuInfoService;
+
+    @GetMapping ("/list/{classId}")
+    @ResponseStatus( HttpStatus.OK )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
+    public SingleResult getStudentInfo(@PathVariable("classId") Long id) {
+        return responseService.getSingleResult(stuInfoService.getStudentInfo(id));
+    }
 }
