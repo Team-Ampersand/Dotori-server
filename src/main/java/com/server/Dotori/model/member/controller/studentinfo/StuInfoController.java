8a9d380c9cf4bb665e4b69b78c55789ab2d4bcf3
qoxogus <a01034367055@gmail.com>
2021-09-13 19:17:02 +0900

    [UPDATE] controller 이름 변경

--- src/main/java/com/server/Dotori/model/member/controller/studentinfo/StuInfoController.java
+++ src/main/java/com/server/Dotori/model/member/controller/studentinfo/StuInfoController.java
@@ -38,7 +38,7 @@ public class StuInfoController {
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
             @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
     })
-    public CommonResult getStudentInfo(@RequestBody RoleUpdateDto roleUpdateDto) {
+    public CommonResult updateRole(@RequestBody RoleUpdateDto roleUpdateDto) {
         stuInfoService.updateRole(roleUpdateDto);
         return responseService.getSuccessResult();
     }
