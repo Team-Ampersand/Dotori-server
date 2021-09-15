ef7529479f5dd15b205378a7017e50fddf7a52c4
노경준 <developerjun0615@gmail.com>
2021-09-15 15:09:12 +0900

    Create MemberDeleteDto.java

---
+++ src/main/java/com/server/Dotori/model/member/dto/MemberDeleteDto.java
@@ -0,0 +1,11 @@
+package com.server.Dotori.model.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDeleteDto {
+    private String username;
+    private String password;
+}
