a0cf8a03e5c8b9b2d843fd4fea6773233d187de5
qoxogus <a01034367055@gmail.com>
2021-09-16 08:56:00 +0900

    [ADD] 쓸모 없는 import제거

--- src/main/java/com/server/Dotori/model/board/controller/admin/AdminBoardController.java
+++ src/main/java/com/server/Dotori/model/board/controller/admin/AdminBoardController.java
@@ -1,7 +1,5 @@
 package com.server.Dotori.model.board.controller.admin;
 
-import com.querydsl.core.Tuple;
-import com.server.Dotori.model.board.Board;
 import com.server.Dotori.model.board.dto.BoardGetDto;
 import com.server.Dotori.model.board.dto.BoardDto;
 import com.server.Dotori.model.board.dto.BoardGetIdDto;
@@ -16,8 +14,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/admin")
