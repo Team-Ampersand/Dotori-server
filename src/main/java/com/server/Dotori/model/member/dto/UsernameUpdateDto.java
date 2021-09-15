1fab7a0237f17adef2870d1dea3703864b1f979a
qoxogus <a01034367055@gmail.com>
2021-09-15 14:15:22 +0900

    [ADD] update username dto

--- src/main/java/com/server/Dotori/model/member/dto/UsernameUpdateDto.java
+++ src/main/java/com/server/Dotori/model/member/dto/UsernameUpdateDto.java
@@ -1,2 +1,11 @@
-package com.server.Dotori.model.member.dto;public class UsernameUpdateDto {
+package com.server.Dotori.model.member.dto;
+
+import lombok.*;
+
+@Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor
+public class UsernameUpdateDto {
+
+    private Long receiverId;
+    private String username;
 }
