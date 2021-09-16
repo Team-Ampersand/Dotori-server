de3a75d1612692e507c5d90390494c4d399a15ab
qoxogus <a01034367055@gmail.com>
2021-09-16 10:37:42 +0900

    [ADD] admim Music controller

---
+++ src/main/java/com/server/Dotori/model/music/controller/admin/AdminMusicController.java
@@ -0,0 +1,56 @@
+package com.server.Dotori.model.music.controller.admin;
+
+import com.server.Dotori.model.music.dto.MusicResDto;
+import com.server.Dotori.model.music.service.MusicService;
+import com.server.Dotori.response.ResponseService;
+import com.server.Dotori.response.result.CommonResult;
+import com.server.Dotori.response.result.SingleResult;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/admin")
+@RequiredArgsConstructor
+public class AdminMusicController {
+
+    private final ResponseService responseService;
+    private final MusicService musicService;
+
+    /**
+     * 음악 신청목록 조회 컨트롤러
+     * @return SingleResult - List - MusicResDto
+     */
+    @GetMapping("/music")
+    @ResponseStatus( HttpStatus.OK )
+    @ApiOperation(value = "음악 신청목록 조회", notes = "음악 신청목록 조회")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
+    public SingleResult<List<MusicResDto>> getAllMusics() {
+        return responseService.getSingleResult(musicService.getAllMusic());
+    }
+
+    /**
+     * 음악신청 목록 개별 삭제 컨트롤러
+     * @param id id
+     * @return CommonResult - SuccessResult
+     */
+    @DeleteMapping("/music/{id}")
+    @ResponseStatus (HttpStatus.NO_CONTENT )
+    @ApiOperation(value = "음악 신청목록 개별 삭제", notes = "음악 신청목록 개별 삭제")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
+    public CommonResult deleteMusic(@PathVariable("id") Long id) {
+        musicService.deleteMusic(id);
+        return responseService.getSuccessResult();
+    }
+}
