42037453708fee62e484723b52db97c792dd4b00
taem1n1 <youman7953@naver.com>
2021-09-16 13:28:30 +0900

    공지사항 관련 Exception

--- src/main/java/com/server/Dotori/exception/board/exception/BoardEmptyException.java
+++ src/main/java/com/server/Dotori/exception/board/exception/BoardEmptyException.java
@@ -1,13 +1,13 @@
 package com.server.Dotori.exception.board.exception;
 
-public class BoardIsEmptyException extends RuntimeException {
-    public BoardIsEmptyException(String msg, Throwable t){
+public class BoardEmptyException extends RuntimeException {
+    public BoardEmptyException(String msg, Throwable t){
         super(msg, t);
     }
-    public BoardIsEmptyException(String msg) {
+    public BoardEmptyException(String msg) {
         super(msg);
     }
-    public BoardIsEmptyException() {
+    public BoardEmptyException() {
         super();
     }
 }
