c167296f2597c1a829bd476dc40f895fc961e0df
taem1n1 <youman7953@naver.com>
2021-09-16 13:16:54 +0900

    Exception 를 Response 하는 Class

--- src/main/java/com/server/Dotori/util/ExceptionResponseObjectUtil.java
+++ src/main/java/com/server/Dotori/util/ExceptionResponseObjectUtil.java
@@ -1,2 +1,25 @@
-package com.server.Dotori.util;public class ExceptionResponseObjectUtil {
+package com.server.Dotori.util;
+
+import com.server.Dotori.response.ResponseService;
+import com.server.Dotori.response.result.CommonResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExceptionResponseObjectUtil {
+
+    private final ResponseService responseService;
+    private final MessageSource messageSource;
+
+    private String getMessage(String code){
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+
+    // *** Exception 처리시 i18n애 정의 되어있는 exception code, msg를 가져온다. ***
+    public CommonResult getExceptionResponseObject(String code){
+        return responseService.getFailResult(Integer.parseInt(getMessage(code + ".code")), getMessage(code + ".msg"));
+    }
 }
