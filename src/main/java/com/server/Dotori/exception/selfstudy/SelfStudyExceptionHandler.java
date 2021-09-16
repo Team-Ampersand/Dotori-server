c7ce62efd7988c0fb41663bb768d82ee3e1ef435
taem1n1 <youman7953@naver.com>
2021-09-16 13:24:38 +0900

    자습신청 관련 Exception 을 Handling하는 Handler

--- src/main/java/com/server/Dotori/exception/selfstudy/SelfStudyExceptionHandler.java
+++ src/main/java/com/server/Dotori/exception/selfstudy/SelfStudyExceptionHandler.java
@@ -1,2 +1,38 @@
-package com.server.Dotori.exception.selfstudy;public class SelfStudyExceptionHandler {
+package com.server.Dotori.exception.selfstudy;
+
+import com.server.Dotori.exception.selfstudy.exception.SelfStudyAlready;
+import com.server.Dotori.exception.selfstudy.exception.SelfStudyCantChange;
+import com.server.Dotori.exception.selfstudy.exception.SelfStudyNotFound;
+import com.server.Dotori.exception.selfstudy.exception.SelfStudyOverPersonal;
+import com.server.Dotori.exception.user.exception.UserAlreadyException;
+import com.server.Dotori.exception.user.exception.UserNotFoundByClassException;
+import com.server.Dotori.exception.user.exception.UserNotFoundException;
+import com.server.Dotori.exception.user.exception.UserPasswordNotMatchingException;
+import com.server.Dotori.response.result.CommonResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public interface SelfStudyExceptionHandler {
+
+    String SELFSTUDY_ALREADY = "selfstudy-already";
+    String SELFSTUDY_CANT_CHANGE = "selfstudy-cant-change";
+    String SELFSTUDY_NOT_FOUNT = "selfstudy-not-found";
+    String SELFSTUDY_OVER_PERSONAL = "selfstudy-over-personal";
+
+    @ExceptionHandler(SelfStudyAlready.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    CommonResult selfStudyAlreadyException(SelfStudyAlready ex);
+
+    @ExceptionHandler(SelfStudyCantChange.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    CommonResult selfStudyCantChangeException(SelfStudyCantChange ex);
+
+    @ExceptionHandler(SelfStudyNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    CommonResult selfStudyNotFoundException(SelfStudyNotFound ex);
+
+    @ExceptionHandler(SelfStudyOverPersonal.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    CommonResult selfStudyOverPersonalException(SelfStudyOverPersonal ex);
 }
--- src/main/java/com/server/Dotori/exception/selfstudy/SelfStudyExceptionHandlerImpl.java
+++ src/main/java/com/server/Dotori/exception/selfstudy/SelfStudyExceptionHandlerImpl.java
@@ -1,2 +1,45 @@
-package com.server.Dotori.exception.selfstudy;public class SelfStudyExceptionHandlerImpl {
+package com.server.Dotori.exception.selfstudy;
+
+import com.server.Dotori.exception.selfstudy.exception.SelfStudyAlready;
+import com.server.Dotori.exception.selfstudy.exception.SelfStudyCantChange;
+import com.server.Dotori.exception.selfstudy.exception.SelfStudyNotFound;
+import com.server.Dotori.exception.selfstudy.exception.SelfStudyOverPersonal;
+import com.server.Dotori.response.result.CommonResult;
+import com.server.Dotori.util.ExceptionResponseObjectUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor @Order(Ordered.HIGHEST_PRECEDENCE)
+@Service
+public class SelfStudyExceptionHandlerImpl implements SelfStudyExceptionHandler {
+
+    private ExceptionResponseObjectUtil exceptionResponseObjectUtil;
+
+    @Override
+    public CommonResult selfStudyNotFoundException(SelfStudyNotFound ex) {
+        log.debug("=== SelfStudy NotFound Exception 발생 ===");
+        return exceptionResponseObjectUtil.getExceptionResponseObject(SELFSTUDY_NOT_FOUNT);
+    }
+
+    @Override
+    public CommonResult selfStudyCantChangeException(SelfStudyCantChange ex) {
+        log.debug("=== SelfStudy Cant Change Exception 발생 ===");
+        return exceptionResponseObjectUtil.getExceptionResponseObject(SELFSTUDY_CANT_CHANGE);
+    }
+
+    @Override
+    public CommonResult selfStudyAlreadyException(SelfStudyAlready ex) {
+        log.debug("=== SelfStudy Already Exception 발생 ===");
+        return exceptionResponseObjectUtil.getExceptionResponseObject(SELFSTUDY_ALREADY);
+    }
+
+    @Override
+    public CommonResult selfStudyOverPersonalException(SelfStudyOverPersonal ex) {
+        log.debug("=== SelfStudy Over Personal Exception 발생 ===");
+        return exceptionResponseObjectUtil.getExceptionResponseObject(SELFSTUDY_OVER_PERSONAL);
+    }
 }
