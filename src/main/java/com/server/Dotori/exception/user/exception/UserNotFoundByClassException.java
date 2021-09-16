92e5f08b2e49d230fe88ce068cc1b793c70ec2a7
taem1n1 <youman7953@naver.com>
2021-09-16 13:18:10 +0900

    User를 학년반별로 조회 했을때 터지는 Exception

--- src/main/java/com/server/Dotori/exception/user/exception/UserNotFoundByClassException.java
+++ src/main/java/com/server/Dotori/exception/user/exception/UserNotFoundByClassException.java
@@ -1,8 +1,13 @@
 package com.server.Dotori.exception.user.exception;
 
 public class UserNotFoundByClassException extends RuntimeException {
-    public UserNotFoundByClass(String msg, Throwable t) {
+    public UserNotFoundByClassException(String msg, Throwable t) {
         super(msg, t);
     }
-    public UserNotFoundException
+    public UserNotFoundByClassException(String msg) {
+        super(msg);
+    }
+    public UserNotFoundByClassException() {
+        super();
+    }
 }
