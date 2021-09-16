63ff765ac679fa39b9eae99b6cbdce0afa8aa92b
taem1n1 <youman7953@naver.com>
2021-09-16 13:28:03 +0900

    공지사항 Exception Controller

--- src/main/java/com/server/Dotori/exception/board/controller/BoardExceptionController.java
+++ src/main/java/com/server/Dotori/exception/board/controller/BoardExceptionController.java
@@ -1,4 +1,4 @@
-package com.server.Dotori.exception.board;
+package com.server.Dotori.exception.board.controller;
 
 import com.server.Dotori.exception.board.exception.*;
 import com.server.Dotori.response.result.CommonResult;
