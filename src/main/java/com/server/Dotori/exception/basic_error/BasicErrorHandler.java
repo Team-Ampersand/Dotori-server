1c42d16ff6555e36f5d2c0c9a6ca180858f301aa
qoxogus <a01034367055@gmail.com>
2021-09-19 19:00:57 +0900

    [FIX] 고쳐진듯

--- src/main/java/com/server/Dotori/exception/basic_error/BasicErrorHandler.java
+++ src/main/java/com/server/Dotori/exception/basic_error/BasicErrorHandler.java
@@ -16,7 +16,6 @@ import static org.springframework.http.HttpStatus.*;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-@RestControllerAdvice
 public class BasicErrorHandler implements ErrorController {
 
     public final static String BASIC_ERROR_BASE_URL = "/error/";
@@ -25,6 +24,12 @@ public class BasicErrorHandler implements ErrorController {
     public final static String FORBIDDEN_403_CODE_NAME = "forbidden";
     public final static String NOT_FOUND_404_CODE_NAME = "not-found";
 
+    /**
+     * BasicError(WhiteLabel) Error가 발생하면
+     * BasicError에 대한 Response객체를 반환하는 CustomErrorController에 정의되어있는 url로 리다이렉트 합니다.
+     * @param request HttpServletRequest
+     * @return BasicErrorController에 정의해놓은 url주소
+     */
     @GetMapping("/error")
     public String handleError(HttpServletRequest request)  {
 
@@ -49,5 +54,4 @@ public class BasicErrorHandler implements ErrorController {
 
         throw new CustomException("basic error처리중 알 수 없는 에러가 발생해 error handling을 하지 못했습니다", INTERNAL_SERVER_ERROR);
     }
-
 }
