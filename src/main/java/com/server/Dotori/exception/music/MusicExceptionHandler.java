6b93e39c6aaf75e820128b2ae478ed0e5b9154e7
taem1n1 <youman7953@naver.com>
2021-09-19 18:30:53 +0900

    찐막 hotfix

--- src/main/java/com/server/Dotori/exception/music/MusicExceptionHandler.java
+++ src/main/java/com/server/Dotori/exception/music/MusicExceptionHandler.java
@@ -23,6 +23,6 @@ public interface MusicExceptionHandler {
     CommonResult musicNotAppliedException(MusicNotAppliedException ex);
 
     @ExceptionHandler(MusicNotFoundException.class)
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     CommonResult musicNotFoundException(MusicNotFoundException ex);
 }
