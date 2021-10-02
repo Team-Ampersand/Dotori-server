df0eff81cd6803a8ba26df0ef2e359c3a7440ffd
qoxogus <a01034367055@gmail.com>
2021-10-02 21:30:46 +0900

    [DELETE] 사용하지 않는 import 삭제

--- src/main/java/com/server/Dotori/model/member/dto/StuNumUpdateDto.java
+++ src/main/java/com/server/Dotori/model/member/dto/StuNumUpdateDto.java
@@ -1,9 +1,6 @@
 package com.server.Dotori.model.member.dto;
 
-import com.server.Dotori.model.member.enumType.Role;
 import lombok.*;
-
-import java.util.List;
 
 @Getter @Setter @Builder
 @AllArgsConstructor @NoArgsConstructor
--- src/main/java/com/server/Dotori/model/music/schedule/MusicSchedule.java
+++ src/main/java/com/server/Dotori/model/music/schedule/MusicSchedule.java
@@ -1,6 +1,5 @@
 package com.server.Dotori.model.music.schedule;
 
-import com.server.Dotori.model.music.repository.MusicRepository;
 import com.server.Dotori.model.music.service.MusicService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
