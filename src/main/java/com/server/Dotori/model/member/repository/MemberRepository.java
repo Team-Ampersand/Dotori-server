e4cb2f15adb436decbc538c247eb70cca30c8c07
노경준 <developerjun0615@gmail.com>
2021-10-15 01:36:00 +0900

    Update MemberRepository findByEmail 메소드 반환값 Member -> Optional<Member> 으로 변경

--- src/main/java/com/server/Dotori/model/member/repository/MemberRepository.java
+++ src/main/java/com/server/Dotori/model/member/repository/MemberRepository.java
@@ -4,10 +4,12 @@ import com.server.Dotori.model.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long>, MemberRepositoryCustom {
     Member findByUsername(String username);
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
     Member findByStdNum(String stdNum);
     boolean existsByUsername(String username);
     boolean existsByStdNum(String stuNum);
