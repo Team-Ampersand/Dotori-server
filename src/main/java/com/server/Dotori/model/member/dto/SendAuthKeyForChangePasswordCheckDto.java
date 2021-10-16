af7cecff5019efbf96037fa245d9b83cbc4f6cf5
노경준 <developerjun0615@gmail.com>
2021-10-16 18:48:12 +0900

    Update SendAuthKeyForChangePassword&CheckDto @qoxogus님 의견 반영

--- src/main/java/com/server/Dotori/model/member/dto/SendAuthKeyForChangePasswordCheckDto.java
+++ src/main/java/com/server/Dotori/model/member/dto/SendAuthKeyForChangePasswordCheckDto.java
@@ -2,12 +2,18 @@ package com.server.Dotori.model.member.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
 public class SendAuthKeyForChangePasswordCheckDto {
+
+    @Pattern(regexp = "^[a-zA-Z0-9]+@gsm.hs.kr$")
+    String email;
+
     @Size(min = 0, max = 6)
     String key;
 
+    @Size(min = 4)
     String newPassword;
 }
--- src/main/java/com/server/Dotori/model/member/dto/SendAuthKeyForChangePasswordDto.java
+++ src/main/java/com/server/Dotori/model/member/dto/SendAuthKeyForChangePasswordDto.java
@@ -1,4 +1,12 @@
 package com.server.Dotori.model.member.dto;
 
-public class sendAuthKeyForChangePasswordDto {
+import lombok.Getter;
+
+import javax.validation.constraints.Pattern;
+
+@Getter
+public class SendAuthKeyForChangePasswordDto {
+
+    @Pattern(regexp = "^[a-zA-Z0-9]+@gsm.hs.kr$")
+    String email;
 }
