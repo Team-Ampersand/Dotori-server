bb739b43e81de65895a4b2f50a6565b5361de70e
qoxogus <a01034367055@gmail.com>
2021-10-16 22:46:22 +0900

    [UPDATE] application server가 시작되는 시간 logging 추가

--- src/main/java/com/server/Dotori/DotoriApplication.java
+++ src/main/java/com/server/Dotori/DotoriApplication.java
@@ -1,13 +1,16 @@
 package com.server.Dotori;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
 import java.util.TimeZone;
 
+@Slf4j
 @EnableScheduling
 @EnableJpaAuditing			//JPA Auditing 활성화
 @SpringBootApplication
@@ -20,6 +23,7 @@ public class DotoriApplication {
 	@PostConstruct
 	void timeZone() {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		log.info("Server Started At {}", new Date());
 	}
 
 	public static void main(String[] args) {
