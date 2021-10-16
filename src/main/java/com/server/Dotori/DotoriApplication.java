bf09534824e6ac798c0de4cfacba437a054b1206
qoxogus <a01034367055@gmail.com>
2021-10-16 22:19:05 +0900

    [ADD] application server 표준시 설정

--- src/main/java/com/server/Dotori/DotoriApplication.java
+++ src/main/java/com/server/Dotori/DotoriApplication.java
@@ -5,10 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @EnableJpaAuditing			//JPA Auditing 활성화
 @SpringBootApplication
 public class DotoriApplication {
+
+	/**
+	 * spring boot application server의 표준시를 한국 표준시로 설정합니다.
+	 * @author 배태현
+	 */
+	@PostConstruct
+	void timeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DotoriApplication.class, args);
