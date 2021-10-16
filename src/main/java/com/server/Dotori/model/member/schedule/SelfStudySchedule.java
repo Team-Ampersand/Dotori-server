4985288c35d9767fe9c3df80cd92877bcc194b04
qoxogus <a01034367055@gmail.com>
2021-10-16 22:19:51 +0900

    [UPDATE] log추가, application server 표준시 설정으로 인한 스케쥴러 zone 삭제

--- src/main/java/com/server/Dotori/model/member/schedule/SelfStudySchedule.java
+++ src/main/java/com/server/Dotori/model/member/schedule/SelfStudySchedule.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 @Slf4j
 @RequiredArgsConstructor
@@ -17,8 +19,9 @@ public class SelfStudySchedule {
      * "월 ~ 금 새벽 2시"에 학생들 자습신청 상태를 자동으로 변경해주는 Schedule
      * @author 배태현
      */
-    @Scheduled(cron = "0 0 2 ? * MON-FRI", zone = "GMT+9")
+    @Scheduled(cron = "0 0 2 ? * MON-FRI")
     public void weekdaySelfStudyStatusReset() {
         selfStudyService.updateSelfStudyStatus();
+        log.info("Student SelfStudy Status Updated At {}", new Date());
     }
 }
