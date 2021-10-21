76e6c7d33b84c2bf7120061d99e48188f47aeb8f
taem1n1 <youman7953@naver.com>
2021-10-21 19:48:55 +0900

    log를 좀더 이쁘게 변경~~

--- src/main/java/com/server/Dotori/security/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/server/Dotori/security/handler/CustomAccessDeniedHandler.java
@@ -16,7 +16,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        log.info("====== Access Denied ======");
+        log.info("=== Access Denied ===");
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 }
