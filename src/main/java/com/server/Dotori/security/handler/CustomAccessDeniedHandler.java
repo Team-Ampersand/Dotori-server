fea39a51bbfc5c48827501d6fa72b113060608cc
taem1n1 <youman7953@naver.com>
2021-10-21 19:43:06 +0900

    Security 설정 보완

---
+++ src/main/java/com/server/Dotori/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.server.Dotori.security.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAccessDenied implements AccessDeniedHandler {
+
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.debug("====== Access Denied ======");
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
---
+++ src/main/java/com/server/Dotori/security/handler/CustomAuthenticationEntryPointHandler.java
@@ -0,0 +1,2 @@
+package com.server.Dotori.security.handler;public class CustomAuthenticationEntryPointHandler {
+}
