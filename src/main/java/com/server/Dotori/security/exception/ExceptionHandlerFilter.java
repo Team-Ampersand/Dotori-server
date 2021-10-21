41e0973321f3ace5a431a0e608ca81383ac66e76
노경준 <developerjun0615@gmail.com>
2021-10-21 09:26:02 +0900

    Update ExceptionHandlerFilter JwtException, IllegalArgumentException, Exception 예외처리 추가

--- src/main/java/com/server/Dotori/security/exception/ExceptionHandlerFilter.java
+++ src/main/java/com/server/Dotori/security/exception/ExceptionHandlerFilter.java
@@ -3,8 +3,11 @@ package com.server.Dotori.security.exception;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.server.Dotori.exception.token.TokenExceptionHandler;
+import com.server.Dotori.exception.token.exception.InvalidTokenException;
+import com.server.Dotori.exception.unknown_exception.UnknownExceptionHandler;
 import com.server.Dotori.response.result.CommonResult;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.persister.collection.OneToManyPersister;
@@ -24,6 +27,7 @@ import java.io.IOException;
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     private final TokenExceptionHandler tokenExceptionHandler;
+    private final UnknownExceptionHandler unknownExceptionHandler;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -32,6 +36,12 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         }
         catch (ExpiredJwtException ex){
             setErrorResponse(HttpStatus.UNAUTHORIZED,response,tokenExceptionHandler.expiredJwtException(ex));
+        }
+        catch (JwtException | IllegalArgumentException ex) {
+            setErrorResponse(HttpStatus.UNAUTHORIZED,response,tokenExceptionHandler.invalidTokenException(new InvalidTokenException()));
+        }
+        catch (Exception ex){
+            setErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,response,unknownExceptionHandler.defaultException(ex));
         }
     }
 
