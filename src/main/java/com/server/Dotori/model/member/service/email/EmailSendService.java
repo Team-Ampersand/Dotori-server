38de0e73f998e5e8ac357f927ed016e560a0a01b
노경준 <developerjun0615@gmail.com>
2021-12-14 23:37:21 +0900

    [DELETE] EmailSendService 사용하지 않는 클래스 삭제

--- src/main/java/com/server/Dotori/model/member/service/email/EmailSendService.java
+++
@@ -1,6 +0,0 @@
-package com.server.Dotori.model.member.service.email;
-
-public interface EmailSendService {
-    void sendEmail(String email,String key);
-    void sendPasswordEmail(String email, String password);
-}
