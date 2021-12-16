fbd5953d537ac2915c74dd02cc716709e8030c82
노경준 <developerjun0615@gmail.com>
2021-12-16 18:25:27 +0900

    [ADD] EmailCertificateDto EmailCertificate 에 값을 넣기 위한 DTO 생성

---
+++ src/main/java/com/server/Dotori/model/member/dto/EmailCertificateDto.java
@@ -0,0 +1,32 @@
+package com.server.Dotori.model.member.dto;
+
+import com.server.Dotori.model.member.EmailCertificate;
+import com.server.Dotori.model.member.Member;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmailCertificateDto {
+
+    private Member member;
+    private String email;
+    private String key;
+    private Boolean expired;
+    private LocalDateTime expiredTime;
+
+    public EmailCertificate toEntity(String email,String key){
+        return EmailCertificate.builder()
+                .member(null)
+                .email(email)
+                .key(key)
+                .expired(false)
+                .expiredTime(LocalDateTime.now().plusMinutes(1))
+                .build();
+    }
+}
