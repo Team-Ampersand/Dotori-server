c032548686b183feee444f58f36ae8f013f167c6
노경준 <developerjun0615@gmail.com>
2021-12-17 15:16:37 +0900

    [Update] EmailCertificateRepository 유효기간이 만료되었을때 해당 키 삭제 가 안되는 에러 해결

--- src/main/java/com/server/Dotori/model/member/repository/email/EmailCertificateRepository.java
+++ src/main/java/com/server/Dotori/model/member/repository/email/EmailCertificateRepository.java
@@ -4,11 +4,14 @@ import com.server.Dotori.model.member.EmailCertificate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
 public interface EmailCertificateRepository extends JpaRepository<EmailCertificate,Long> {
     Optional<EmailCertificate> findByKey(String key);
+    @Transactional
     void deleteEmailCertificateByKey(String key);
+    @Transactional
     void deleteEmailCertificateByEmail(String email);
 }
