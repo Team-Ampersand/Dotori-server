2ec1605ba6af73243e7e4846218a953467f7dcf5
노경준 <developerjun0615@gmail.com>
2021-12-17 14:13:28 +0900

    [Update] EmailCertificateRepository deleteEmailCertificateByEmail 추가

--- src/main/java/com/server/Dotori/model/member/repository/email/EmailCertificateRepository.java
+++ src/main/java/com/server/Dotori/model/member/repository/email/EmailCertificateRepository.java
@@ -4,8 +4,11 @@ import com.server.Dotori.model.member.EmailCertificate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmailCertificateRepository extends JpaRepository<EmailCertificate,Long> {
-    EmailCertificate findByKey(String key);
+    Optional<EmailCertificate> findByKey(String key);
     void deleteEmailCertificateByKey(String key);
+    void deleteEmailCertificateByEmail(String email);
 }
