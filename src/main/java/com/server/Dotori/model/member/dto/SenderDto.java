22ae5bbdaa13aca1ca827e7c00f50091f3f73feb
노경준 <developerjun0615@gmail.com>
2021-12-17 12:09:58 +0900

    [Update] SenderDto 사용하지 않는 import 삭제

--- src/main/java/com/server/Dotori/model/member/dto/SenderDto.java
+++ src/main/java/com/server/Dotori/model/member/dto/SenderDto.java
@@ -4,9 +4,6 @@ import com.amazonaws.services.simpleemail.model.*;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 public class SenderDto {
 
