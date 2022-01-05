f9be9cbc6af1f6ce76c652c0d01bc28fcf424aa4
qoxogus <a01034367055@gmail.com>
2022-01-06 08:52:58 +0900

    [UPDATE] 공지사항 작성, 삭제 로직 변경

--- src/main/java/com/server/Dotori/model/board/service/BoardService.java
+++ src/main/java/com/server/Dotori/model/board/service/BoardService.java
@@ -6,10 +6,13 @@ import com.server.Dotori.model.board.dto.BoardDto;
 import com.server.Dotori.model.board.dto.BoardGetIdDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface BoardService {
 
-    Board createBoard(BoardDto boardDto);
+    Board createBoard(BoardDto boardDto, MultipartFile multipartFileList);
 
     Page<BoardGetDto> getAllBoard(Pageable pageable);
 
