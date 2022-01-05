13eccf37d55eae33a5a7506f4dd4bec5a56421dd
qoxogus <a01034367055@gmail.com>
2022-01-06 08:34:55 +0900

    [UPDATE] toEntity url, url null 넘어올 시 url null로 지정

--- src/main/java/com/server/Dotori/model/board/dto/BoardDto.java
+++ src/main/java/com/server/Dotori/model/board/dto/BoardDto.java
@@ -5,6 +5,7 @@ import com.server.Dotori.model.member.Member;
 import lombok.*;
 
 import javax.validation.constraints.*;
+import java.util.List;
 
 @Getter @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,11 +20,18 @@ public class BoardDto {
     @Size(min = 1, max = 500)
     private String content;
 
-    public Board saveToEntity(Member member) {
-        return Board.builder()
+    public Board saveToEntity(Member member, String url) {
+        Board board = Board.builder()
                 .title(title)
                 .content(content)
                 .member(member)
+                .url("https://dotori-s3.s3.ap-northeast-2.amazonaws.com/img/" + url)
                 .build();
+        try {
+            if (url.isEmpty()) board.setUrl(null);
+        } catch (NullPointerException e) {
+            board.setUrl(null);
+        }
+        return board;
     }
 }
