446b507791f4c1abe929d73f763e11e6b58b9446
qoxogus <a01034367055@gmail.com>
2022-01-05 10:24:51 +0900

    [UPDATE] 날짜입력받는 Dto 제약조건 추가

--- src/main/java/com/server/Dotori/model/music/dto/DateMusicDto.java
+++ src/main/java/com/server/Dotori/model/music/dto/DateMusicDto.java
@@ -2,11 +2,13 @@ package com.server.Dotori.model.music.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Getter @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 public class DateMusicDto {
 
+    @NotBlank
     private LocalDate date;
 }
