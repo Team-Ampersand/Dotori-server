aaaecb0bab83bc1d9418241feaa5e2a4400034f6
qoxogus <a01034367055@gmail.com>
2022-01-06 08:52:32 +0900

    [ADD] s3 service

--- src/main/java/com/server/Dotori/model/board/service/S3Service.java
+++ src/main/java/com/server/Dotori/model/board/service/S3Service.java
@@ -45,11 +45,11 @@ public class S3Service {
         amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
     }
 
-    private String createFileName(String fileName) { // 먼저 파일 업로드 시, 파일명을 난수화하기 위해 random으로 돌립니다.
+    private String createFileName(String fileName) {
         return UUID.randomUUID().toString().concat(getFileExtension(fileName));
     }
 
-    private String getFileExtension(String fileName) { // file 형식이 잘못된 경우를 확인하기 위해 만들어진 로직이며, 파일 타입과 상관없이 업로드할 수 있게 하기 위해 .의 존재 유무만 판단하였습니다.
+    private String getFileExtension(String fileName) {
         try {
             return fileName.substring(fileName.lastIndexOf("."));
         } catch (StringIndexOutOfBoundsException e) {
