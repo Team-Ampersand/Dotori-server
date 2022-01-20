a84ed4860160e00875f60419e1d7bd552489c439
노경준 <developerjun0615@gmail.com>
2022-01-20 22:25:47 +0900

    [Update] RuleCntAndDatesDto 수정했습니다.

--- src/main/java/com/server/Dotori/model/rule/dto/RulesCntAndDatesDto.java
+++ src/main/java/com/server/Dotori/model/rule/dto/RulesCntAndDatesDto.java
@@ -1,17 +1,18 @@
 package com.server.Dotori.model.rule.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class RulesCntAndDatesDto {
 
     private int cnt;
     private List<String> date;
 
-    public RulesCntAndDatesDto(int cnt, List<String> date) {
-        this.cnt = cnt;
-        this.date = date;
-    }
 }
