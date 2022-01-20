dcad3cf24e662c9fa7352ea60f2cfbde30b035d0
노경준 <developerjun0615@gmail.com>
2022-01-20 18:30:20 +0900

    [Update] RuleService 기숙사 규정을 어긴 학생의 이력을 조회하는 기능 구현

--- src/main/java/com/server/Dotori/model/rule/service/RuleService.java
+++ src/main/java/com/server/Dotori/model/rule/service/RuleService.java
@@ -1,9 +1,12 @@
 package com.server.Dotori.model.rule.service;
 
 import com.server.Dotori.model.rule.dto.RuleGrantDto;
+import com.server.Dotori.model.rule.dto.RulesCntAndDatesDto;
+import com.server.Dotori.model.rule.enumType.Rule;
 
-import java.util.List;
+import java.util.HashMap;
 
 public interface RuleService {
     void grant(RuleGrantDto ruleGrantDto);
+    HashMap<Rule, RulesCntAndDatesDto> findViolationOfTheRules(String stuNum);
 }
