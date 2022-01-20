55ff749161d22fb132cc2bbd54021e49940c711b
노경준 <developerjun0615@gmail.com>
2022-01-20 18:31:35 +0900

    [Update] RuleViolation 기숙사 규정을 어긴 학생의 이력을 조회하는 기능 구현

--- src/main/java/com/server/Dotori/model/rule/RuleViolation.java
+++ src/main/java/com/server/Dotori/model/rule/RuleViolation.java
@@ -1,5 +1,6 @@
 package com.server.Dotori.model.rule;
 
+import com.server.Dotori.model.BaseTimeEntity;
 import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.rule.enumType.Rule;
 import lombok.*;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Builder @Getter
 @Entity @Table(name = "RuleViolation")
-public class RuleViolation {
+public class RuleViolation extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "rule_violation_id")
