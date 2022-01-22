05b299beb227cc6f76e6c1f1e580c21ef40bf6ce
taem1n1 <youman7953@naver.com>
2022-01-22 18:54:36 +0900

    안마의자 서비스로직 [안마의자 신청]

--- src/main/java/com/server/Dotori/model/massage/service/MassageService.java
+++ src/main/java/com/server/Dotori/model/massage/service/MassageService.java
@@ -1,2 +1,8 @@
-package com.server.Dotori.model.massage.service;public class MassageService {
+package com.server.Dotori.model.massage.service;
+
+import java.time.DayOfWeek;
+
+public interface MassageService {
+    void requestMassage(DayOfWeek dayOfWeek, int hour);
+
 }
--- src/main/java/com/server/Dotori/model/massage/service/MassageServiceImpl.java
+++ src/main/java/com/server/Dotori/model/massage/service/MassageServiceImpl.java
@@ -1,2 +1,46 @@
-package com.server.Dotori.model.massage.service;public class MassageServiceImpl {
+package com.server.Dotori.model.massage.service;
+
+import com.server.Dotori.model.massage.repository.MassageRepository;
+import com.server.Dotori.model.member.Member;
+import com.server.Dotori.model.member.enumType.Massage;
+import com.server.Dotori.util.CurrentMemberUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.DayOfWeek;
+
+import static com.server.Dotori.model.member.enumType.Massage.*;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MassageServiceImpl implements MassageService {
+
+    private final MassageRepository massageRepository;
+    private final CurrentMemberUtil currentMemberUtil;
+
+    @Override
+    public void requestMassage(DayOfWeek dayOfWeek, int hour) {
+        if (dayOfWeek == DayOfWeek.MONDAY || dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) throw new IllegalArgumentException("안마의자를 신청 할 수 있는 요일이 아닙니다.");
+        if (!(hour >= 20 && hour <= 21)) throw new IllegalArgumentException("안마의자 신청을 하실 수 있는 시간이 아닙니다.");
+
+        long count = massageRepository.count();
+
+        if (count < 5) {
+            Member currentMember = currentMemberUtil.getCurrentMember();
+
+            if (currentMember.getMassage() == CAN) {
+                massageRepository.save(com.server.Dotori.model.massage.Massage.builder()
+                    .member(currentMember)
+                    .build()
+                );
+                currentMember.updateMassage(APPLIED);
+
+                log.info("Current MassageRequest Student Count is {}", count+1);
+            } else throw new IllegalArgumentException("안마의자 신청을 이미 하셨습니다.");
+        } else throw new IllegalArgumentException("안마의자 신청 가능 인원이 초과 하였습니다");
+    }
 }
