4c7d0f6eb904d3318ad4781add94231c2d5ff2b3
taem1n1 <youman7953@naver.com>
2022-01-22 18:56:18 +0900

    안마의자 신청 테스트케이스 작성

--- src/test/java/com/server/Dotori/model/member/service/massage/MassageServiceTest.java
+++ src/test/java/com/server/Dotori/model/member/service/massage/MassageServiceTest.java
@@ -1,41 +1,49 @@
 package com.server.Dotori.model.member.service.massage;
 
 import com.server.Dotori.model.massage.repository.MassageRepository;
+import com.server.Dotori.model.massage.service.MassageService;
 import com.server.Dotori.model.member.dto.MemberDto;
+import com.server.Dotori.model.member.enumType.Massage;
 import com.server.Dotori.model.member.enumType.Role;
+import com.server.Dotori.model.member.repository.member.MemberRepository;
 import com.server.Dotori.util.CurrentMemberUtil;
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.DayOfWeek;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
-public class MassageService {
-    
+public class MassageServiceTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired PasswordEncoder passwordEncoder;
     @Autowired CurrentMemberUtil currentMemberUtil;
     @Autowired MassageService massageService;
     @Autowired MassageRepository massageRepository;
 
     @BeforeEach
-    @DisplayName("로그인 되어있는 유저를 확인하는 테스트")
-    void currentUser() {
-        //given
+    void signupMember() {
         MemberDto memberDto = MemberDto.builder()
-                .memberName("배태현")
-                .stuNum("2409")
-                .password("0809")
-                .email("s20032@gsm.hs.kr")
+                .memberName("김태민")
+                .stuNum("2406")
+                .password("1234")
+                .email("s20014@gsm.hs.kr")
                 .build();
         memberDto.setPassword(passwordEncoder.encode(memberDto.getPassword()));
         memberRepository.save(memberDto.toEntity());
@@ -53,6 +61,15 @@ public class MassageService {
 
         //then
         String currentMemberEmail = CurrentMemberUtil.getCurrentEmail();
-        assertEquals("s20032@gsm.hs.kr", currentMemberEmail);
+        assertEquals("s20014@gsm.hs.kr", currentMemberEmail);
+    }
+
+    @Test
+    @DisplayName("안마의자 신청이 잘 되나요?")
+    public void requestMassageTest() {
+        massageService.requestMassage(DayOfWeek.WEDNESDAY, 20);
+
+        assertThat(Massage.APPLIED).isEqualTo(currentMemberUtil.getCurrentMember().getMassage());
+        assertThat(1).isEqualTo(massageRepository.count());
     }
 }
