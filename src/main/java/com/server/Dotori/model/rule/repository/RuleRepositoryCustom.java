1437a59c366c31354b884b5b242a6be866e36e28
노경준 <developerjun0615@gmail.com>
2022-01-24 07:28:53 +0900

    [Update] RuleRepositoryCustom 단일 학생 규정위반 정보 단일 조회 기능 개발

--- src/main/java/com/server/Dotori/model/rule/repository/RuleRepositoryCustom.java
+++ src/main/java/com/server/Dotori/model/rule/repository/RuleRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.server.Dotori.model.rule.repository;
 
-import com.server.Dotori.model.rule.dto.FindRulesAndDatesDto;
+import com.server.Dotori.model.rule.dto.FindIdAndRuleAndDateDto;
+import com.server.Dotori.model.rule.dto.FindRuleAndDateDto;
 
 import java.util.List;
 
 public interface RuleRepositoryCustom {
-    List<FindRulesAndDatesDto> findViolationOfTheRules(String stuNum);
+    List<FindRuleAndDateDto> findViolationOfTheRule(String stuNum);
+
+    List<FindIdAndRuleAndDateDto> findViolationOfTheRules(String stuNum);
 }
