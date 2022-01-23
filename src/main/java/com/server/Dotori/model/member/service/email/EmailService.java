bfc2d5db3ccb77f65a9550e65fe94510cb4c736f
노경준 <developerjun0615@gmail.com>
2022-01-23 11:15:27 +0900

    [Update] Email 레거시 청산

--- src/main/java/com/server/Dotori/model/member/service/email/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.server.Dotori.model.member.service.email;
-
-import com.server.Dotori.model.member.dto.EmailDto;
-import com.server.Dotori.model.member.dto.MemberEmailKeyDto;
-
-public interface EmailService {
-    String authKey(EmailDto emailDto);
-    String authCheck(MemberEmailKeyDto memberEmailKeyDto);
-}
