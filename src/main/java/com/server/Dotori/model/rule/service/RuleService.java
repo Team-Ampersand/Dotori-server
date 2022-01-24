642061ca0ee741d93e5bbd77fba1f8268e420359
노경준 <developerjun0615@gmail.com>
2022-01-24 13:18:27 +0900

    [Update] RuleService response 값 변경

--- src/main/java/com/server/Dotori/model/rule/service/RuleService.java
+++ src/main/java/com/server/Dotori/model/rule/service/RuleService.java
@@ -1,6 +1,6 @@
 package com.server.Dotori.model.rule.service;
 
-import com.server.Dotori.model.rule.dto.FindIdAndRuleAndDateDto;
+import com.server.Dotori.model.rule.dto.FindViolationOfTheRuleResponseDto;
 import com.server.Dotori.model.rule.dto.RuleGrantDto;
 import com.server.Dotori.model.rule.dto.RulesCntAndDatesDto;
 import com.server.Dotori.model.rule.enumType.Rule;
@@ -11,5 +11,5 @@ import java.util.List;
 public interface RuleService {
     void grant(RuleGrantDto ruleGrantDto);
     HashMap<Rule, RulesCntAndDatesDto> findAllViolationOfTheRule(String stuNum);
-    List<FindIdAndRuleAndDateDto> findViolationOfTheRules(String stuNum);
+    List<FindViolationOfTheRuleResponseDto> findViolationOfTheRules(String stuNum);
 }
