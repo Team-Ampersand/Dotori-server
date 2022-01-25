fe8ef2598c96e60e89976e96daa7a9578c145a59
taem1n1 <youman7953@naver.com>
2022-01-26 02:01:11 +0900

    각종 안마의자 신청 테스트 케이스

--- src/test/java/com/server/Dotori/model/member/service/massage/MassageServiceTest.java
+++ src/test/java/com/server/Dotori/model/member/service/massage/MassageServiceTest.java
@@ -1,5 +1,7 @@
 package com.server.Dotori.model.member.service.massage;
 
+import com.server.Dotori.exception.massage.exception.MassageCantRequestDateException;
+import com.server.Dotori.exception.massage.exception.MassageCantRequestTimeException;
 import com.server.Dotori.model.massage.repository.MassageRepository;
 import com.server.Dotori.model.massage.service.MassageService;
 import com.server.Dotori.model.member.dto.MemberDto;
@@ -7,13 +9,16 @@ import com.server.Dotori.model.member.enumType.Massage;
 import com.server.Dotori.model.member.enumType.Role;
 import com.server.Dotori.model.member.repository.member.MemberRepository;
 import com.server.Dotori.util.CurrentMemberUtil;
-import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.config.CronTask;
+import org.springframework.scheduling.config.ScheduledTask;
+import org.springframework.scheduling.config.ScheduledTaskHolder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
@@ -23,9 +28,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -36,6 +43,7 @@ public class MassageServiceTest {
     @Autowired CurrentMemberUtil currentMemberUtil;
     @Autowired MassageService massageService;
     @Autowired MassageRepository massageRepository;
+    @Autowired ScheduledTaskHolder scheduledTaskHolder;
 
     @BeforeEach
     void signupMember() {
@@ -74,5 +82,32 @@ public class MassageServiceTest {
 
         assertThat(Massage.APPLIED).isEqualTo(currentMemberUtil.getCurrentMember().getMassage());
         assertThat(1).isEqualTo(massageRepository.count());
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("안마의자 신청 예외가 제대로 터지나요?")
+    public void massageExceptionTest() {
+        assertThrows(
+                MassageCantRequestDateException.class,
+                () -> massageService.requestMassage(DayOfWeek.FRIDAY, 20, 20));
+
+        assertThrows(
+                MassageCantRequestTimeException.class,
+                () -> massageService.requestMassage(DayOfWeek.MONDAY, 19, 19));
+    }
+
+    @Test
+    @DisplayName("자정에 안마의자 신청 상태가 변경되는지 확인하는 테스트")
+    public void everyNightMassageStatusChangeTest() {
+        Set<ScheduledTask> scheduledTasks = scheduledTaskHolder.getScheduledTasks();
+        scheduledTasks.forEach(scheduledTask -> scheduledTask.getTask().getRunnable().getClass().getDeclaredMethods());
+
+        long count = scheduledTasks.stream()
+                .filter(scheduledTask -> scheduledTask.getTask() instanceof CronTask)
+                .map(scheduledTask -> (CronTask) scheduledTask.getTask())
+                .filter(cronTask -> cronTask.getExpression().equals("0 0 0 * * ?") && cronTask.toString().equals("com.server.Dotori.model.massage.schedule.MassageSchedule.updateMassageStatus"))
+                .count();
+        Assertions.assertThat(count).isEqualTo(1L);
     }
 }
