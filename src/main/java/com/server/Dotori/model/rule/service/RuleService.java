f313b8f0aa8ab21707b1d642807e84145b9ed35f
노경준 <developerjun0615@gmail.com>
2022-01-25 22:57:15 +0900

    [Update] RuleService @PathVariable 로 변경

--- src/main/java/com/server/Dotori/model/rule/service/RuleService.java
+++ src/main/java/com/server/Dotori/model/rule/service/RuleService.java
@@ -13,5 +13,5 @@ public interface RuleService {
     void grant(RuleGrantDto ruleGrantDto);
     HashMap<Rule, RulesCntAndDatesDto> findAllViolationOfTheRule(String stuNum);
     List<FindViolationOfTheRuleResponseDto> findViolationOfTheRules(String stuNum);
-    void deleteViolationOfTheRules(DeleteViolationOfTheRulesDto deleteViolationOfTheRulesDto);
+    void deleteViolationOfTheRules(Long id);
 }
