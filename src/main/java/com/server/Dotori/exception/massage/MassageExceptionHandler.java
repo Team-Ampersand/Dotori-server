516335e1fb35b10d414889da68296a7b8604a5e7
taem1n1 <youman7953@naver.com>
2022-01-26 01:55:24 +0900

    안마의자 관련 예외처리 핸들러

--- src/main/java/com/server/Dotori/exception/massage/MassageExceptionHandler.java
+++ src/main/java/com/server/Dotori/exception/massage/MassageExceptionHandler.java
@@ -1,16 +1,20 @@
 package com.server.Dotori.exception.massage;
 
 import com.server.Dotori.exception.massage.exception.MassageAlreadyException;
+import com.server.Dotori.exception.massage.exception.MassageCantRequestDateException;
+import com.server.Dotori.exception.massage.exception.MassageCantRequestTimeException;
 import com.server.Dotori.exception.massage.exception.MassageOverException;
 import com.server.Dotori.response.result.CommonResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-public interface MassageHandler {
+public interface MassageExceptionHandler {
 
     String MASSAGE_ALREADY = "massage-already";
     String MASSAGE_OVER = "massage-over";
+    String MASSAGE_CANT_REQUEST_DATE = "massage-cant-request-date";
+    String MASSAGE_CANT_REQUEST_TIME = "massage-cant-request-time";
 
     @ExceptionHandler(MassageAlreadyException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
@@ -19,4 +23,12 @@ public interface MassageHandler {
     @ExceptionHandler(MassageOverException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     CommonResult massageOverException(MassageOverException ex);
+
+    @ExceptionHandler(MassageCantRequestDateException.class)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    CommonResult massageCantRequestDateException(MassageCantRequestDateException ex);
+
+    @ExceptionHandler(MassageCantRequestTimeException.class)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    CommonResult massageCantRequestTimeException(MassageCantRequestTimeException ex);
 }
