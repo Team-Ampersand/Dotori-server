5c4b6eeb20d9d4468932f125fa33d26218dcebd1
taem1n1 <youman7953@naver.com>
2022-01-26 02:04:13 +0900

    develop에서 developer로 변경

--- src/main/java/com/server/Dotori/model/massage/controller/developer/DeveloperMassageController.java
+++ src/main/java/com/server/Dotori/model/massage/controller/developer/DeveloperMassageController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 
 @RestController
-@RequestMapping("/v1/develop")
+@RequestMapping("/v1/developer")
 @RequiredArgsConstructor
 public class DeveloperMassageController {
 
