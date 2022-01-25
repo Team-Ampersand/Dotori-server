88c8649040a0794d6fcce64abb42b5b6ee176494
taem1n1 <youman7953@naver.com>
2022-01-26 02:00:52 +0900

    안마의자 신청 상세로직 추가

--- src/main/java/com/server/Dotori/model/massage/service/MassageServiceImpl.java
+++ src/main/java/com/server/Dotori/model/massage/service/MassageServiceImpl.java
@@ -1,15 +1,21 @@
 package com.server.Dotori.model.massage.service;
 
+import com.server.Dotori.exception.massage.exception.MassageAlreadyException;
+import com.server.Dotori.exception.massage.exception.MassageCantRequestDateException;
+import com.server.Dotori.exception.massage.exception.MassageCantRequestTimeException;
+import com.server.Dotori.exception.massage.exception.MassageOverException;
 import com.server.Dotori.model.massage.repository.MassageRepository;
 import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.member.enumType.Massage;
 import com.server.Dotori.util.CurrentMemberUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 
 import static com.server.Dotori.model.member.enumType.Massage.*;
 
@@ -24,9 +30,9 @@ public class MassageServiceImpl implements MassageService {
 
     @Override
     public void requestMassage(DayOfWeek dayOfWeek, int hour, int min) {
-        if (dayOfWeek == DayOfWeek.MONDAY || dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) throw new IllegalArgumentException("안마의자를 신청 할 수 있는 요일이 아닙니다.");
-        if (!(hour >= 20 && hour <= 21)) throw new IllegalArgumentException("안마의자 신청을 하실 수 있는 시간이 아닙니다.");
-        if (!(min >= 20)) throw new IllegalArgumentException("안마의자 신청을 하실 수 있는 시간이 아닙니다.");
+        if (dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) throw new MassageCantRequestDateException();
+        if (!(hour >= 20 && hour <= 21)) throw new MassageCantRequestTimeException();
+        if (!(min >= 20)) throw new MassageCantRequestTimeException();
 
         long count = massageRepository.count();
 
@@ -39,9 +45,19 @@ public class MassageServiceImpl implements MassageService {
                     .build()
                 );
                 currentMember.updateMassage(APPLIED);
+                currentMember.updateMassageExpiredDate(LocalDateTime.now().plusMonths(1));
 
                 log.info("Current MassageRequest Student Count is {}", count+1);
-            } else throw new IllegalArgumentException("안마의자 신청을 이미 하셨습니다.");
-        } else throw new IllegalArgumentException("안마의자 신청 가능 인원이 초과 하였습니다");
+            } else throw new MassageAlreadyException();
+        } else throw new MassageOverException();
+    }
+
+    @Override
+    public void updateMassageStatus() {
+        Member currentMember = currentMemberUtil.getCurrentMember();
+
+        if (currentMember.getMassageExpiredDate().isBefore(LocalDateTime.now())) {
+            currentMember.updateMassage(CAN);
+        }
     }
 }
