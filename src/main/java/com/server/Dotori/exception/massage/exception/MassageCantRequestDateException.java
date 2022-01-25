6057ec059a2b85e4394d064021b746c40592966e
taem1n1 <youman7953@naver.com>
2022-01-26 01:56:46 +0900

    해당 요일에 신청 할 수 없다고 하는 Massage exception

--- src/main/java/com/server/Dotori/exception/massage/exception/MassageCantRequestDateException.java
+++ src/main/java/com/server/Dotori/exception/massage/exception/MassageCantRequestDateException.java
@@ -1,13 +1,13 @@
 package com.server.Dotori.exception.massage.exception;
 
 public class MassageCantRequestDateException extends RuntimeException {
-    public MassageCantRequestDate(String msg, Throwable t) {
+    public MassageCantRequestDateException(String msg, Throwable t) {
         super(msg, t);
     }
-    public MassageCantRequestDate(String msg) {
+    public MassageCantRequestDateException(String msg) {
         super(msg);
     }
-    public MassageCantRequestDate() {
+    public MassageCantRequestDateException() {
         super();
     }
 }
