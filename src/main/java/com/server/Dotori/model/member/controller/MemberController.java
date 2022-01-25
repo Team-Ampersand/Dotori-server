2171a7542135f744f8b06cd62dcc32120eec1073
노경준 <developerjun0615@gmail.com>
2022-01-25 23:47:41 +0900

    [Update] MemberController Sign 부분 나누기

--- src/main/java/com/server/Dotori/model/member/controller/MemberController.java
+++ src/main/java/com/server/Dotori/model/member/controller/MemberController.java
@@ -21,58 +21,6 @@ public class MemberController {
     private final ResponseService responseService;
 
     /**
-     * 회원가입 Controller
-     * @param memberDto username, stdNum, password, email, answer
-     * @return SuccessResult
-     * @author 노경준
-     */
-    @PostMapping("/signup")
-    @ApiOperation(value="회원가입")
-    public CommonResult signup(@Valid @RequestBody MemberDto memberDto){
-        memberService.signup(memberDto);
-        return responseService.getSuccessResult();
-    }
-
-    /**
-     * 회원가입 이메일 인증 Controller
-     * @param emailDto email
-     * @return SuccessResult
-     * @author 노경준
-     */
-    @PostMapping("/signup/email")
-    @ApiOperation(value="회원가입 이메일 인증")
-    public CommonResult sendEmailSignup(@Valid @RequestBody EmailDto emailDto){
-        memberService.sendEmailSignup(emailDto);
-        return responseService.getSuccessResult();
-    }
-
-    /**
-     * 회원가입 이메일 인증 확인 Controller
-     * @param memberEmailKeyDto key
-     * @return SuccessResult
-     * @author 노경준
-     */
-    @PostMapping("/signup/email/check")
-    @ApiOperation(value="회원가입 이메일 인증 확인")
-    public CommonResult checkEmailSignup(@Valid @RequestBody SignUpEmailCheckDto memberEmailKeyDto){
-        memberService.checkEmailSignup(memberEmailKeyDto);
-        return responseService.getSuccessResult();
-    }
-
-    /**
-     * 로그인 Controller
-     * @param memberLoginDto email, password
-     * @return SingleResult
-     * @author 노경준
-     */
-    @PostMapping("/signin")
-    @ApiOperation(value="로그인")
-    public SingleResult<Map<String, String>> signin(@Valid @RequestBody SignInDto memberLoginDto){
-        Map<String, String> data = memberService.signIn(memberLoginDto);
-        return responseService.getSingleResult(data);
-    }
-
-    /**
      * 비밀번호 변경 Controller
      * @param memberPasswordDto oldPassword, newPassword
      * @return SingleResult
