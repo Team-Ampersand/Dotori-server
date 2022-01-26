ab6a10b4466f221ea594f88cbaed2b7cf4e22b56
taem1n1 <youman7953@naver.com>
2022-01-27 00:53:40 +0900

    권한별 안마의자 신청 현황 조회

--- src/main/java/com/server/Dotori/model/massage/controller/admin/AdminMassageController.java
+++ src/main/java/com/server/Dotori/model/massage/controller/admin/AdminMassageController.java
@@ -1,4 +1,35 @@
 package com.server.Dotori.model.massage.controller.admin;
 
+import com.server.Dotori.model.massage.service.MassageService;
+import com.server.Dotori.response.ResponseService;
+import com.server.Dotori.response.result.SingleResult;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/admin")
+@RequiredArgsConstructor
 public class AdminMassageController {
+
+    private final MassageService massageService;
+    private final ResponseService responseService;
+
+    @GetMapping("/massage")
+    @ResponseStatus( HttpStatus.OK )
+    @ApiOperation(value = "안마의자 신청한 학생 전체 조회", notes = "안마의자 신청한 학생 전체 조회")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
+    public SingleResult getMassageStudentsAdmin() {
+        return responseService.getSingleResult(massageService.getMassageStudents());
+    }
+
 }
