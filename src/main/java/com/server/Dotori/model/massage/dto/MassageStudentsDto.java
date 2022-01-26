82bef79455fe8a4dd26b5d010d3f9bd73af4a4f2
taem1n1 <youman7953@naver.com>
2022-01-27 00:53:09 +0900

    조회시 보여지는 entity 스펙

--- src/main/java/com/server/Dotori/model/massage/dto/MassageStudentsDto.java
+++ src/main/java/com/server/Dotori/model/massage/dto/MassageStudentsDto.java
@@ -1,2 +1,15 @@
-package com.server.Dotori.model.massage.dto;public class MassageStudentsDto {
+package com.server.Dotori.model.massage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MassageStudentsDto {
+
+    private Long id;
+    private String stuNum;
+    private String memberName;
 }
