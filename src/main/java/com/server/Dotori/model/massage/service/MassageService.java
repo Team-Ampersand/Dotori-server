3d99762d4b273440af9eb48c9ee7d70c150873d1
taem1n1 <youman7953@naver.com>
2022-01-27 00:52:19 +0900

    안마의자 신청한 학생을 조회하는 service 추가

--- src/main/java/com/server/Dotori/model/massage/service/MassageService.java
+++ src/main/java/com/server/Dotori/model/massage/service/MassageService.java
@@ -1,9 +1,13 @@
 package com.server.Dotori.model.massage.service;
 
+import com.server.Dotori.model.massage.dto.MassageStudentsDto;
+
 import java.time.DayOfWeek;
+import java.util.List;
 
 public interface MassageService {
     void requestMassage(DayOfWeek dayOfWeek, int hour, int min);
     void cancelMassage(DayOfWeek dayOfWeek, int hour, int min);
     void updateMassageStatus();
+    List<MassageStudentsDto> getMassageStudents();
 }
