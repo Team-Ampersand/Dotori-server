fb4a0efa3d2e072a02850760770bac457c2ef95a
taem1n1 <youman7953@naver.com>
2022-01-26 21:32:32 +0900

    안마의자 신청 취소 추가

--- src/main/java/com/server/Dotori/model/massage/service/MassageService.java
+++ src/main/java/com/server/Dotori/model/massage/service/MassageService.java
@@ -4,5 +4,6 @@ import java.time.DayOfWeek;
 
 public interface MassageService {
     void requestMassage(DayOfWeek dayOfWeek, int hour, int min);
+    void cancelMassage(DayOfWeek dayOfWeek, int hour, int min);
     void updateMassageStatus();
 }
