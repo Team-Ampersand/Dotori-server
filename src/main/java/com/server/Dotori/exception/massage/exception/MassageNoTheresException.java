e09e2500fe51b5daef86872035c9c68837cbbc36
taem1n1 <youman7953@naver.com>
2022-01-27 00:54:25 +0900

    새로운 Exception

--- src/main/java/com/server/Dotori/exception/massage/exception/MassageNoTheresException.java
+++ src/main/java/com/server/Dotori/exception/massage/exception/MassageNoTheresException.java
@@ -1,2 +1,13 @@
-package com.server.Dotori.exception.massage.exception;public class MassageNoThersException {
+package com.server.Dotori.exception.massage.exception;
+
+public class MassageNoTheresException extends RuntimeException{
+    public MassageNoTheresException(String msg, Throwable t) {
+        super(msg, t);
+    }
+    public MassageNoTheresException(String msg) {
+        super(msg);
+    }
+    public MassageNoTheresException() {
+        super();
+    }
 }
