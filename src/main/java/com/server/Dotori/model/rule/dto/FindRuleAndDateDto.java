c0a6c6dcccd5e4484ccf491c136fdf4861da126e
노경준 <developerjun0615@gmail.com>
2022-02-01 22:14:51 +0900

    [Update] FindRuleAndDateDto 날짜 컬럼 추가로 인한 로직 변경

--- src/main/java/com/server/Dotori/model/rule/dto/FindRuleAndDateDto.java
+++ src/main/java/com/server/Dotori/model/rule/dto/FindRuleAndDateDto.java
@@ -4,13 +4,13 @@ import com.server.Dotori.model.rule.enumType.Rule;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @Getter
 public class FindRuleAndDateDto {
 
     private Rule rules;
-    private LocalDateTime dates;
+    private LocalDate dates;
 
 }
