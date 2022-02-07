a94cf1f673ca46b5d7491c292097bfe1a7187d8d
qoxogus <a01034367055@gmail.com>
2022-02-07 20:04:53 +0900

    [UPDATE] 제약조건 추가

--- src/main/java/com/server/Dotori/model/member/dto/MemberNameUpdateDto.java
+++ src/main/java/com/server/Dotori/model/member/dto/MemberNameUpdateDto.java
@@ -2,10 +2,15 @@ package com.server.Dotori.model.member.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter @Setter @Builder
 @AllArgsConstructor @NoArgsConstructor
 public class MemberNameUpdateDto {
 
+    @NotBlank
     private Long receiverId;
+
+    @NotBlank
     private String memberName;
 }
