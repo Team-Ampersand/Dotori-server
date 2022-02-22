aff579efdfecb76f864e13ff775f69221f8697ea
taem1n1 <youman7953@naver.com>
2022-02-22 16:00:48 +0900

    Exception Handler 작성

--- src/main/java/com/server/Dotori/exception/rule/RuleExceptionHandler.java
+++ src/main/java/com/server/Dotori/exception/rule/RuleExceptionHandler.java
@@ -1,2 +1,16 @@
-package com.server.Dotori.exception.rule;public class RuleExceptionHandler {
+package com.server.Dotori.exception.rule;
+
+import com.server.Dotori.exception.rule.exception.RuleNoHistoryException;
+import com.server.Dotori.response.result.CommonResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public interface RuleExceptionHandler {
+
+    String RULE_NO_HISTORY = "rule-no-history";
+
+    @ExceptionHandler(RuleNoHistoryException.class)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    CommonResult ruleNoHistoryException(RuleNoHistoryException ex);
 }
