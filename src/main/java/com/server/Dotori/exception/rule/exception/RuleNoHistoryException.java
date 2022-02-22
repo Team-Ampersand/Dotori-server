d9d65d650a4a7b3c3d2817d59ea0153f5321b401
taem1n1 <youman7953@naver.com>
2022-02-22 16:00:57 +0900

    Exception 추가

--- src/main/java/com/server/Dotori/exception/rule/exception/RuleNoHistoryException.java
+++ src/main/java/com/server/Dotori/exception/rule/exception/RuleNoHistoryException.java
@@ -1,2 +1,15 @@
-package com.server.Dotori.exception.rule.exception;public class RuleNoHistoryException {
+package com.server.Dotori.exception.rule.exception;
+
+public class RuleNoHistoryException extends RuntimeException {
+
+    public RuleNoHistoryException(String msg, Throwable t) {
+        super(msg, t);
+    }
+    public RuleNoHistoryException(String msg) {
+        super(msg);
+    }
+    public RuleNoHistoryException() {
+        super();
+    }
+
 }
