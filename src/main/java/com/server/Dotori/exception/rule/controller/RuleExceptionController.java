fca085b1de9e2ec4d927fa0771b4d7912833fec6
taem1n1 <youman7953@naver.com>
2022-02-22 16:00:31 +0900

    Exception Controller 작성

--- src/main/java/com/server/Dotori/exception/rule/controller/RuleExceptionController.java
+++ src/main/java/com/server/Dotori/exception/rule/controller/RuleExceptionController.java
@@ -1,2 +1,18 @@
-package com.server.Dotori.exception.rule.controller;public class RuleExceptionController {
+package com.server.Dotori.exception.rule.controller;
+
+import com.server.Dotori.exception.rule.exception.RuleNoHistoryException;
+import com.server.Dotori.response.result.CommonResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exception")
+public class RuleExceptionController {
+
+    @GetMapping("/rule-no-history")
+    public CommonResult ruleNoHistoryException() {
+        throw new RuleNoHistoryException();
+    }
+
 }
