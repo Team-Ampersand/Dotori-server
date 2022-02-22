9ffb43de271e16e814b7a190bf4489763c2be522
노경준 <developerjun0615@gmail.com>
2022-02-22 23:51:33 +0900

    [Update] CouncillorRuleController 메인 페이지 에서 규정위반을 조회할 수 있는 컨트롤러 추가

---
+++ src/main/java/com/server/Dotori/model/rule/controller/councillor/CouncillorRuleController.java
@@ -0,0 +1,31 @@
+package com.server.Dotori.model.rule.controller.councillor;
+
+import com.server.Dotori.model.rule.dto.FindViolationOfTheRuleResponseDto;
+import com.server.Dotori.model.rule.service.RuleService;
+import com.server.Dotori.response.ResponseService;
+import com.server.Dotori.response.result.SingleResult;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/v1/councillor/rule")
+public class CouncillorRuleController {
+
+    private final RuleService ruleService;
+    private final ResponseService responseService;
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = false, dataType = "String", paramType = "header"),
+    })
+    @GetMapping("/main")
+    public SingleResult<List<FindViolationOfTheRuleResponseDto>> findRuleAtMainPage(){
+        return responseService.getSingleResult(ruleService.findRuleAtMainPage());
+    }
+}
