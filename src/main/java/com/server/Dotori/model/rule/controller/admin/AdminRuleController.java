319fcbc982ca21a7add84dc535e825411eea3425
노경준 <developerjun0615@gmail.com>
2022-02-25 22:45:29 +0900

    [Update] AdminRuleController Admin 권한에서 학생정보 전체 조회 기능 추가

--- src/main/java/com/server/Dotori/model/rule/controller/admin/AdminRuleController.java
+++ src/main/java/com/server/Dotori/model/rule/controller/admin/AdminRuleController.java
@@ -1,12 +1,11 @@
 package com.server.Dotori.model.rule.controller.admin;
 
-import com.server.Dotori.model.rule.dto.FindViolationOfTheRuleResponseDto;
-import com.server.Dotori.model.rule.dto.RuleGrantDto;
-import com.server.Dotori.model.rule.dto.RulesCntAndDatesDto;
+import com.server.Dotori.model.rule.dto.*;
 import com.server.Dotori.model.rule.enumType.Rule;
 import com.server.Dotori.model.rule.service.RuleService;
 import com.server.Dotori.response.ResponseService;
 import com.server.Dotori.response.result.CommonResult;
+import com.server.Dotori.response.result.ListResult;
 import com.server.Dotori.response.result.SingleResult;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -14,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -56,5 +54,31 @@ public class AdminRuleController {
     public CommonResult deleteViolationOfTheRules(@PathVariable Long id){
         ruleService.deleteViolationOfTheRules(id);
         return responseService.getSuccessResult();
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+    })
+    @GetMapping("/stuinfo")
+    public ListResult<FindStusDto> findALLStudents() {
+        return responseService.getListResult(ruleService.findAllStudents());
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
+    @GetMapping("/stuinfo/{classId}")
+    public ListResult<FindStusDto> findStusByClassId(@PathVariable("classId") Long id){
+        return responseService.getListResult(ruleService.findStusByClassId(id));
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
+    @GetMapping("/stuinfo/members")
+    public ListResult<FindStusDto> findStusByMemberName(@RequestParam(value = "membername", required = true) String memberName){
+        return responseService.getListResult(ruleService.findStusByMemberName(memberName));
     }
 }
