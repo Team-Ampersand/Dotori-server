533052b787fc9c75ea80166dcf94251c15cdd6b1
qoxogus <a01034367055@gmail.com>
2022-03-05 20:39:05 +0900

    [UPDATE] 클라이언트에서 음악신청 처리 방식이 바뀌었으므로 서버에서 매달마다 음악신청 내역을 초기화 할 필요없어서 삭제

--- src/main/java/com/server/Dotori/model/music/schedule/MusicSchedule.java
+++ src/main/java/com/server/Dotori/model/music/schedule/MusicSchedule.java
@@ -15,26 +15,6 @@ public class MusicSchedule {
 
     private final MusicService musicService;
 
-//    /**
-//     * "토요일 23시 59분"에 음악신청 목록을 자동으로 초기화해주는 Scheduled
-//     * @author 배태현
-//     */
-//    @Scheduled(cron = "0 59 23 ? * SAT")
-//    public void saturdayMusicDeleteAll() {
-//        musicService.saturdayMusicDeleteAll();
-//        log.info("Music Delete All At {}", new Date());
-//    }
-
-    /**
-     * 매 달 1일 새벽 4시에 음악신청 목록을 초기화해주는 Scheduled
-     * @author 배태현
-     */
-    @Scheduled(cron = "0 0 4 1 1/1 ?")
-    public void monthMusicDeleteAll() {
-        musicService.monthMusicDeleteAll();
-        log.info("Music Delete All At {}", new Date());
-    }
-
     /**
      * "월 ~ 금 자정"에 음악신청 상태를 자동으로 update 해주는 Scheduled
      * @author 배태현
@@ -44,16 +24,4 @@ public class MusicSchedule {
         musicService.updateMemberMusicStatus();
         log.info("Student Music Status Updated At {}", new Date());
     }
-
-//    /**
-//     * 스케줄을 테스트한 메서드 1분마다 log를 찍어준다
-//     * ex @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Sat Oct 16 22:08:00 KST 2021
-//     *    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Sat Oct 16 22:09:00 KST 2021
-//     *    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Sat Oct 16 22:10:00 KST 2021
-//     */
-//    @Scheduled(cron = "0 0/1 * 1/1 * ?")
-//    public void schedulerTest() {
-//        log.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ {}", new Date());
-//    }
-
 }
