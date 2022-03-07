ce8e38f3bc6e954183c1e36230f23ee11dedc78e
taem1n1 <youman7953@naver.com>
2022-03-07 14:04:12 +0900

    Dotori 코드 전역에서 사용되는 DotoriException Handler

--- src/main/java/com/server/Dotori/new_exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/server/Dotori/new_exception/handler/GlobalExceptionHandler.java
@@ -1,24 +1,19 @@
 package com.server.Dotori.new_exception.handler;
 
+import com.server.Dotori.new_exception.DotoriException;
 import com.server.Dotori.new_exception.ErrorResponse;
-import com.server.Dotori.new_exception.exception.massage.MassageAlreadyException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class DotoriExceptionHandler {
+public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(MassageAlreadyException.class)
-//    public ResponseEntity<ErrorResponse> handleMassageAlreadyException(MassageAlreadyException ex) {
-//        log.error("Handle Massage Already Exception", ex);
-//        ErrorResponse response = new ErrorResponse(ex.getErrorCode());
-//        return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
-//    }
+    @ExceptionHandler(value = {DotoriException.class})
+    public ResponseEntity<ErrorResponse> handleDotoriException(DotoriException e) {
+        log.error("handleCustomException throw DotoriException : {}", e.getErrorCode());
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
 }
