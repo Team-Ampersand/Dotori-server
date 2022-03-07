9d1fbb07172cf7c3a0b4fb4a6e4adf7cc920ba2d
taem1n1 <youman7953@naver.com>
2022-03-07 14:05:22 +0900

    response할 error 메세지 스펙(양식)

--- src/main/java/com/server/Dotori/new_exception/ErrorResponse.java
+++ src/main/java/com/server/Dotori/new_exception/ErrorResponse.java
@@ -1,2 +1,35 @@
-package com.server.Dotori.new_exception;public class ErrorResponse {
+package com.server.Dotori.new_exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+    private final LocalDateTime timeStamp = LocalDateTime.now();
+    private final int status;
+    private final String error;
+    private final String message;
+    private final String details;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getHttpStatus().value())
+                        .error(errorCode.getHttpStatus().name())
+                        .message(errorCode.getMessage())
+                        .details(errorCode.getDetails())
+                        .build()
+                );
+    }
+
+
+
 }
