6a832e799e0974517c9821c8f0f5dfa6ce270bfd
taem1n1 <youman7953@naver.com>
2022-03-07 23:50:15 +0900

    실수로 손댄 레거시 코드들

--- src/main/java/com/server/Dotori/security/exception/ExceptionHandlerFilter.java
+++ src/main/java/com/server/Dotori/security/exception/ExceptionHandlerFilter.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.server.Dotori.exception.token.TokenExceptionHandler;
 import com.server.Dotori.exception.token.exception.InvalidTokenException;
+import com.server.Dotori.new_exception.DotoriException;
+import com.server.Dotori.new_exception.ErrorCode;
+import com.server.Dotori.new_exception.ErrorResponse;
 import com.server.Dotori.new_exception.handler.UnknownExceptionHandler;
 import com.server.Dotori.response.result.CommonResult;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.entity.ContentType;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -20,12 +24,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.server.Dotori.new_exception.ErrorCode.*;
+
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
-    private final TokenExceptionHandler tokenExceptionHandler;
+    private final ObjectMapper objectMapper;
     private final UnknownExceptionHandler unknownExceptionHandler;
 
     @Override
@@ -34,20 +40,27 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
         } catch (ExpiredJwtException ex) {
             log.debug("================= [ ExceptionHandlerFilter ] 에서 ExpiredJwtException 발생 ===================");
-            setErrorResponse(HttpStatus.UNAUTHORIZED, response, tokenExceptionHandler.expiredJwtException(ex));
+            setErrorResponse(EXPIRED_TOKEN, response);
         } catch (JwtException | IllegalArgumentException ex) {
             log.debug("================= [ ExceptionHandlerFilter ] 에서 JwtException 발생 ===================");
-            setErrorResponse(HttpStatus.UNAUTHORIZED, response, tokenExceptionHandler.invalidTokenException(new InvalidTokenException()));
+            setErrorResponse(INVALID_TOKEN, response);
+        } catch (Exception ex) {
+            log.debug("================= [ ExceptionHandlerFilter ] 에서 Exception 발생 ===================");
+            setErrorResponse(UNKNOWN_ERROR, response);
         }
     }
 
-    public void setErrorResponse(HttpStatus status, HttpServletResponse response, CommonResult commonResult) throws IOException {
-        response.setStatus(status.value());
+    public void setErrorResponse(ErrorCode errorCode, HttpServletResponse response) throws IOException {
+        response.setStatus(errorCode.getHttpStatus().value());
         response.setContentType("application/json; charset=utf-8");
-        response.getWriter().write(throwableResultToJson(commonResult));
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(errorCode.getHttpStatus().value())
+                .error(errorCode.getHttpStatus().name())
+                .message(errorCode.getMessage())
+                .build();
+        String errorResponseEntityToJson = objectMapper.writeValueAsString(errorResponse);
+        response.getWriter().write(errorResponseEntityToJson.toString());
     }
 
-    private String throwableResultToJson(CommonResult ex) throws JsonProcessingException {
-        return new ObjectMapper().writeValueAsString(ex);
-    }
 }
