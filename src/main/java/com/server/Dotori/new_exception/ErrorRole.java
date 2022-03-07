146612f0444e88ad83fe64624125fa14832cdd22
taem1n1 <youman7953@naver.com>
2022-03-07 14:04:42 +0900

    Error 메세지가 어느 권한에서 터진지 확인하게 해주는 Enum

--- src/main/java/com/server/Dotori/new_exception/ErrorRole.java
+++ src/main/java/com/server/Dotori/new_exception/ErrorRole.java
@@ -1,2 +1,10 @@
-package com.server.Dotori.new_exception;public class ErrorRole {
+package com.server.Dotori.new_exception;
+
+/**
+ * STUDENT : 사감쌤을 제외한 Dotori 서비스를 이용하는 모든 학생
+ * ALL : 사감쌤과 학생 모두를 포함
+ * SERVER : 서버 에러
+ */
+public enum ErrorRole {
+    SERVER, ALL, STUDENT
 }
