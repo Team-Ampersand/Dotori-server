5079fa16d6f432ae80af2e2f265fba7b0de51467
qoxogus <a01034367055@gmail.com>
2022-03-08 21:22:40 +0900

    [UPDATE] self-study feature 변경된 exception으로 변경

--- src/main/java/com/server/Dotori/model/member/service/selfstudy/SelfStudyServiceImpl.java
+++ src/main/java/com/server/Dotori/model/member/service/selfstudy/SelfStudyServiceImpl.java
@@ -1,12 +1,11 @@
 package com.server.Dotori.model.member.service.selfstudy;
 
-import com.server.Dotori.exception.member.exception.MemberNotFoundByClassException;
 import com.server.Dotori.exception.member.exception.MemberNotFoundException;
-import com.server.Dotori.exception.selfstudy.exception.*;
 import com.server.Dotori.model.member.Member;
 import com.server.Dotori.model.member.dto.SelfStudyStudentsDto;
 import com.server.Dotori.model.member.repository.member.MemberRepository;
 import com.server.Dotori.model.member.repository.selfStudy.SelfStudyRepository;
+import com.server.Dotori.new_exception.DotoriException;
 import com.server.Dotori.util.CurrentMemberUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 import static com.server.Dotori.model.member.enumType.SelfStudy.*;
+import static com.server.Dotori.new_exception.ErrorCode.*;
 
 @Service
 @Slf4j
@@ -38,17 +38,17 @@ public class SelfStudyServiceImpl implements SelfStudyService {
      * 자습신청 할 시 '신청함'으로 상태변경 <br>
      * @param dayOfWeek 현재 요일
      * @param hour 현재 시
-     * @exception SelfStudyCantRequestDateException 금요일, 토요일, 일요일에 자습신청을 했을 때
-     * @exception SelfStudyCantRequestTimeException 오후 8시에서 오후 9시 사이가 아닌 시간에 자습신청을 했을 때
-     * @exception SelfStudyCantAppliedException 자습신청 상태가 CAN(가능)이 아닐 때 (자습신청을 할 수 없는 상태)
-     * @exception SelfStudyOverPersonalException 자습신청 인원이 50명이 넘었을 때
+     * @exception DotoriException (SELF_STUDY_CANT_REQUEST_DATE) 금요일, 토요일, 일요일에 자습신청을 했을 때
+     * @exception DotoriException (SELF_STUDY_CANT_REQUEST_TIME) 오후 8시에서 오후 9시 사이가 아닌 시간에 자습신청을 했을 때
+     * @exception DotoriException (SELF_STUDY_ALREADY) 자습신청 상태가 CAN(가능)이 아닐 때 (자습신청을 할 수 없는 상태)
+     * @exception DotoriException (SELF_STUDY_OVER) 자습신청 인원이 50명이 넘었을 때
      * @author 배태현
      */
     @Override
     @Transactional
     public void requestSelfStudy(DayOfWeek dayOfWeek, int hour) {
-        if (dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) throw new SelfStudyCantRequestDateException();
-        if (!(hour >= 20 && hour < 21)) throw new SelfStudyCantRequestTimeException(); // 20시(8시)부터 21시(9시 (8시 59분)) 사이가 아니라면 자습신청 불가능
+        if (dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) throw new DotoriException(SELF_STUDY_CANT_REQUEST_DATE);
+        if (!(hour >= 20 && hour < 21)) throw new DotoriException(SELF_STUDY_CANT_REQUEST_TIME); // 20시(8시)부터 21시(9시 (8시 59분)) 사이가 아니라면 자습신청 불가능
 
         Member currentMember = currentMemberUtil.getCurrentMember();
         long count = selfStudyRepository.count();
@@ -63,9 +63,9 @@ public class SelfStudyServiceImpl implements SelfStudyService {
 
                 log.info("Current Self Study Student Count is {}", count+1);
             } else
-                throw new SelfStudyCantAppliedException();
+                throw new DotoriException(SELF_STUDY_ALREADY);
         } else
-            throw new SelfStudyOverPersonalException();
+            throw new DotoriException(SELF_STUDY_OVER);
     }
 
     /**
@@ -75,16 +75,16 @@ public class SelfStudyServiceImpl implements SelfStudyService {
      * 자습신청을 취소할 시 그 날 자습신청 불가능
      * @param dayOfWeek 현재 요일
      * @param hour 현재 시
-     * @exception SelfStudyCantCancelDateException 금요일, 토요일, 일요일에 자습신청 취소를 했을 때
-     * @exception SelfStudyCantCancelTimeException 오후 8시에서 오후 9시 사이가 아닌 시간에 자습신청 취소를 했을 때
-     * @exception SelfStudyCantChangeException 자습신청 상태가 APPLIED(신청됨)가 아닐 때 (자습신청 취소를 할 수 없는 상태)
+     * @exception DotoriException (SELF_STUDY_CANT_CANCEL_DATE) 금요일, 토요일, 일요일에 자습신청 취소를 했을 때
+     * @exception DotoriException (SELF_STUDY_CANT_REQUEST_TIME) 오후 8시에서 오후 9시 사이가 아닌 시간에 자습신청 취소를 했을 때
+     * @exception DotoriException (SELF_STUDY_CANT_CANCEL) 자습신청 상태가 APPLIED(신청됨)가 아닐 때 (자습신청 취소를 할 수 없는 상태)
      * @author 배태현
      */
     @Override
     @Transactional
     public void cancelSelfStudy(DayOfWeek dayOfWeek, int hour) {
-        if (dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) throw new SelfStudyCantCancelDateException();
-        if (!(hour >= 20 && hour < 21)) throw new SelfStudyCantCancelTimeException(); // 20시(8시)부터 21시(9시 (8시 59분)) 사이가 아니라면 자습신청 취소 불가능
+        if (dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) throw new DotoriException(SELF_STUDY_CANT_CANCEL_DATE);
+        if (!(hour >= 20 && hour < 21)) throw new DotoriException(SELF_STUDY_CANT_REQUEST_TIME); // 20시(8시)부터 21시(9시 (8시 59분)) 사이가 아니라면 자습신청 취소 불가능
 
         Member currentMember = currentMemberUtil.getCurrentMember();
         long count = selfStudyRepository.count();
@@ -94,20 +94,20 @@ public class SelfStudyServiceImpl implements SelfStudyService {
             selfStudyRepository.deleteByMemberId(currentMember.getId());
             log.info("Current Self Study Student Count is {}", count-1);
         } else
-            throw new SelfStudyCantChangeException();
+            throw new DotoriException(SELF_STUDY_CANT_CANCEL);
     }
 
     /**
      * 자습신청한 학생을 전체 조회하는 서비스로직 (로그인된 유저 사용가능)
      * @return List - SelfStudyStudentDto (id, stuNum, username)
-     * @exception SelfStudyNotFoundException 자습신청한 학생이 없을 때
+     * @exception DotoriException (SELF_STUDY_NOT_FOUND) 자습신청한 학생이 없을 때
      * @author 배태현
      */
     @Override
     public List<SelfStudyStudentsDto> getSelfStudyStudents() {
         List<SelfStudyStudentsDto> selfStudyAPLLIED = memberRepository.findBySelfStudyAPLLIED();
 
-        if (selfStudyAPLLIED.isEmpty()) throw new SelfStudyNotFoundException();
+        if (selfStudyAPLLIED.isEmpty()) throw new DotoriException(SELF_STUDY_NOT_FOUND);
         return selfStudyAPLLIED;
     }
 
@@ -115,14 +115,14 @@ public class SelfStudyServiceImpl implements SelfStudyService {
      * 자습신청한 학생을 학년반별로 조회하는 서비스로직 (로그인된 유저 사용가능)
      * @param id classId
      * @return List - SelfStudyStudentDto (id, stuNum, username)
-     * @exception MemberNotFoundByClassException 해당 반에 해당하는 학생이 없을 때
+     * @exception DotoriException (MEMBER_NOT_FOUND_BY_CLASS) 해당 반에 해당하는 학생이 없을 때
      * @author 배태현
      */
     @Override
     public List<SelfStudyStudentsDto> getSelfStudyStudentsByCategory(Long id) {
         List<SelfStudyStudentsDto> selfStudyCategory = memberRepository.findBySelfStudyCategory(id);
 
-        if (selfStudyCategory.isEmpty()) throw new MemberNotFoundByClassException();
+        if (selfStudyCategory.isEmpty()) throw new DotoriException(MEMBER_NOT_FOUND_BY_CLASS);
 
         return selfStudyCategory;
     }
