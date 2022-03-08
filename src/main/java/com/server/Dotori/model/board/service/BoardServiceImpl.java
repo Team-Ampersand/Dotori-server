1e41d6b756ba501a8e8d44068eb02fcf0907b003
qoxogus <a01034367055@gmail.com>
2022-03-08 20:42:24 +0900

    [UPDATE] 변경된 exception으로 변경

--- src/main/java/com/server/Dotori/model/board/service/BoardServiceImpl.java
+++ src/main/java/com/server/Dotori/model/board/service/BoardServiceImpl.java
@@ -1,14 +1,12 @@
 package com.server.Dotori.model.board.service;
 
-import com.server.Dotori.exception.board.exception.BoardEmptyException;
-import com.server.Dotori.exception.board.exception.BoardNotFoundException;
 import com.server.Dotori.model.board.Board;
-import com.server.Dotori.model.board.dto.BoardGetDto;
 import com.server.Dotori.model.board.dto.BoardDto;
+import com.server.Dotori.model.board.dto.BoardGetDto;
 import com.server.Dotori.model.board.dto.BoardGetIdDto;
 import com.server.Dotori.model.board.repository.BoardRepository;
 import com.server.Dotori.model.member.Member;
-
+import com.server.Dotori.new_exception.DotoriException;
 import com.server.Dotori.util.CurrentMemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -17,6 +15,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import static com.server.Dotori.new_exception.ErrorCode.BOARD_EMPTY;
+import static com.server.Dotori.new_exception.ErrorCode.BOARD_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -47,7 +48,7 @@ public class BoardServiceImpl implements BoardService {
     /**
      * 공지사항 전체조회 서비스 로직 (로그인 되어있는 유저 사용가능)
      * @param pageable pageble(7)
-     * @exception BoardEmptyException 작성된 공지사항이 없을 때
+     * @exception DotoriException (BOARD_EMPTY) 작성된 공지사항이 없을 때
      * @return Page-BoardGetDto (id, title, roles, createdDate)
      * @author 배태현
      */
@@ -55,7 +56,7 @@ public class BoardServiceImpl implements BoardService {
     public Page<BoardGetDto> getAllBoard(Pageable pageable) {
         Page<Board> boardPage = boardRepository.findAll(pageable);
 
-        if (boardPage.isEmpty()) throw new BoardEmptyException();
+        if (boardPage.isEmpty()) throw new DotoriException(BOARD_EMPTY);
 
         return boardPage.map(board -> {
             ModelMapper mapper = new ModelMapper();
@@ -69,7 +70,7 @@ public class BoardServiceImpl implements BoardService {
     /**
      * 공지사항 상세조회 서비스 로직 (로그인 되어있을 시 사용가능)
      * @param boardId boardId
-     * @exception BoardNotFoundException 해당 Id의 공지사항을 찾을 수 없을 때
+     * @exception DotoriException (BOARD_NOT_FOUND) 해당 Id의 공지사항을 찾을 수 없을 때
      * @return BoardGetDto (id, title, content,  roles, createdDate, modifiedDate)
      * @author 배태현
      */
@@ -77,7 +78,7 @@ public class BoardServiceImpl implements BoardService {
     public BoardGetIdDto getBoardById(Long boardId) {
 
         Board board = boardRepository.findById(boardId)
-                .orElseThrow(() -> new BoardNotFoundException());
+                .orElseThrow(() -> new DotoriException(BOARD_NOT_FOUND));
 
         ModelMapper modelMapper = new ModelMapper();
         BoardGetIdDto map = modelMapper.map(board, BoardGetIdDto.class);
@@ -90,7 +91,7 @@ public class BoardServiceImpl implements BoardService {
      * 공지사항 수정 서비스 로직 (기자위, 사감쌤, 개발자 권한 사용가능)
      * @param boardId boardId
      * @param boardUpdateDto boardUpdateDto (title, content)
-     * @exception BoardNotFoundException 해당 Id의 공지사항을 찾을 수 없을 때
+     * @exception DotoriException (BOARD_NOT_FOUND) 해당 Id의 공지사항을 찾을 수 없을 때
      * @return Board
      * @author 배태현
      */
@@ -98,7 +99,7 @@ public class BoardServiceImpl implements BoardService {
     @Transactional
     public Board updateBoard(Long boardId, BoardDto boardUpdateDto) {
         Board board = boardRepository.findById(boardId)
-                .orElseThrow(() -> new BoardNotFoundException());
+                .orElseThrow(() -> new DotoriException(BOARD_NOT_FOUND));
 
         board.updateBoard(boardUpdateDto.getTitle(), boardUpdateDto.getContent());
 
@@ -108,13 +109,13 @@ public class BoardServiceImpl implements BoardService {
     /**
      * 공지사항 삭제 서비스 로직 (기자위, 사감쌤, 개발자 권한 사용가능)
      * @param boardId boardId
-     * @exception BoardNotFoundException 해당 Id의 공지사항을 찾을 수 없을 때
+     * @exception DotoriException (BOARD_NOT_FOUND) 해당 Id의 공지사항을 찾을 수 없을 때
      * @author 배태현
      */
     @Override
     public void deleteBoard(Long boardId) {
         Board board = boardRepository.findById(boardId)
-                .orElseThrow(() -> new BoardNotFoundException());
+                .orElseThrow(() -> new DotoriException(BOARD_NOT_FOUND));
 
         try {
             s3Service.deleteFile(board.getUrl().substring(54));
