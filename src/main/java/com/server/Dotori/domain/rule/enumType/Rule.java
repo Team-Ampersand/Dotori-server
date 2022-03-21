12a5de7a6d6a3a6283fda5fe0b79b8b561251d1e
노경준 <developerjun0615@gmail.com>
2022-03-21 15:20:50 +0900

    [Update] Rule 대분류 이름 변경

--- src/main/java/com/server/Dotori/domain/rule/enumType/Rule.java
+++ src/main/java/com/server/Dotori/domain/rule/enumType/Rule.java
@@ -12,13 +12,13 @@ public enum Rule {
     ALCOHOL("금지 물품 반입"),
     TOBACCO("금지 물품 반입"),
     MEANDERING_APPARATUS("금지 물품 반입"),
-    FOOD("금지 물품 반입"),                                                   // 1. 금지 물품 반입
+    FOOD("금지 물품 반입"),                                                  // 1. 금지 물품 반입
 
-    MANAGER_GUIDANCE("사감 지도 불이행"),                                     // 2. 사감 지도 불이행
+    MANAGER_GUIDANCE("사감 지도 불이행"),                                    // 2. 사감 지도 불이행
 
     TIME("시간 관 소홀 및 이탈 행위"),
     OUTING("시간 관 소홀 및 이탈 행위"),
-    OVERNIGHT_STAY("시간 관 소홀 및 이탈 행위"),                               // 3. 시간 관 소홀 및 이탈 행위 ( 타호실 )
+    OVERNIGHT_STAY("시간 관 소홀 및 이탈 행위"),                               // 3. 시간 관 소홀 및 이탈 행위
 
     DAMAGE_OF_GOODS("물품 훼손 및 절도"),
     THEFT("물품 훼손 및 절도"),
@@ -34,16 +34,16 @@ public enum Rule {
     DAMAGE_OF_POST("공동 생활 방해 및 위생 상태 불량"),
     POSSESSION_OF_ELECTRONICS_DEVICES("공동 생활 방해 및 위생 상태 불량"),
     CLEAN_COUNDITION_BAD("공동 생활 방해 및 위생 상태 불량"),
-    ENVIRONMENT_POLLUTION("공동 생활 방해 및 위생 상태 불량"),                      // 6. 공동 생활 방해 및 위생 상태 불량
+    ENVIRONMENT_POLLUTION("공동 생활 방해 및 위생 상태 불량"),                  // 6. 공동 생활 방해 및 위생 상태 불량
 
-    AFFECTIONATE_ACT("애정 행위 금지"),
-    SEXUAL_ACT("애정 행위 금지"),                                               // 7. 애정 행위 금지
+    AFFECTIONATE_ACT("애정 행위"),
+    SEXUAL_ACT("애정 행위"),                                               // 7. 애정 행위
 
-    ENTRY_TO_PROHIBITED_AREAS("기숙사 출입금지 구역을 허가받지 않고 이성 기숙사 구역에 출입한 경우"),                                                  // 8. 기숙사 출입금지 구역을 허가받지 않고 이성 기숙사 구역에 출입한 경우
+    ENTRY_TO_PROHIBITED_AREAS("기숙사 출입 금지 구역 출입"),                  // 8. 기숙사 출입 금지 구역 출입
 
-    VIOLATION_OF_STUDY_ROOM_RULES("학습실에서 면학분위기를 저해할 경우"),                                              // 9. 학습실에서 면학분위기를 저해할 경우
+    VIOLATION_OF_STUDY_ROOM_RULES("학습실 면학분위기 저해"),                 // 9. 학습실에서 면학분위기 저해
 
-    OUTSIDER_ENTRY("외부인을 출입 시키는 경우");                                                              // 10. 외부인을 출입 시키는 경우
+    OUTSIDER_ENTRY("외부인을 출입 관여");                                   // 10. 외부인 출입 관여
 
     private String big;
 
