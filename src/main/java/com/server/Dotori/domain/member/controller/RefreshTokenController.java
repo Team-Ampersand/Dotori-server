fe4920c607d8b23a59955a9cfa117b4f3f0cf09b
노경준 <developerjun0615@gmail.com>
2022-03-25 16:08:44 +0900

    [Update] RefreshTokenController Token 을 Refresh 하는 로직 변경

--- src/main/java/com/server/Dotori/domain/member/controller/RefreshTokenController.java
+++ src/main/java/com/server/Dotori/domain/member/controller/RefreshTokenController.java
@@ -1,5 +1,6 @@
 package com.server.Dotori.domain.member.controller;
 
+import com.server.Dotori.domain.member.dto.RefreshTokenDto;
 import com.server.Dotori.domain.member.service.RefreshTokenService;
 import com.server.Dotori.global.response.ResponseService;
 import com.server.Dotori.global.response.result.SingleResult;
@@ -8,10 +9,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -27,17 +25,17 @@ public class RefreshTokenController {
 
     /**
      * 토큰 재발급 Controller
-     * @param request refreshToken
+     * @param request refreshToken, refreshRequestDto
      * @return SingleResult
-     * @author 노경준, 배태현
+     * @author 노경준
      */
     @PutMapping("/refresh")
     @ResponseStatus( HttpStatus.OK )
     @ApiImplicitParams({
             @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = true, dataType = "String", paramType = "header")
     })
-    public SingleResult<Map<String, String>> refresh(HttpServletRequest request){
-        Map<String, String> data = refreshTokenService.getRefreshToken(jwtTokenProvider.resolveRefreshToken(request));
+    public SingleResult<Map<String, String>> refresh(HttpServletRequest request, @RequestBody RefreshTokenDto refreshTokenDto){
+        Map<String, String> data = refreshTokenService.refreshToken(jwtTokenProvider.resolveRefreshToken(request), refreshTokenDto);
         return responseService.getSingleResult(data);
     }
 
