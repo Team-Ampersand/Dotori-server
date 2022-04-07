89538ab69835374f8c4349402a959fe3d5f9d26d
노경준 <developerjun0615@gmail.com>
2022-04-07 14:40:26 +0900

    [Update] RuleGrantDto 권한부여 여러명 가능하도록 변경

--- src/main/java/com/server/Dotori/domain/rule/dto/RuleGrantDto.java
+++ src/main/java/com/server/Dotori/domain/rule/dto/RuleGrantDto.java
@@ -1,7 +1,5 @@
 package com.server.Dotori.domain.rule.dto;
 
-import com.server.Dotori.domain.member.Member;
-import com.server.Dotori.domain.rule.RuleViolation;
 import com.server.Dotori.domain.rule.enumType.Rule;
 import lombok.*;
 
@@ -11,19 +9,11 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-@Setter
 @Getter
 public class RuleGrantDto {
 
     private List<String> stuNum;
-    private Rule rule;
+    private List<Rule> rule;
     private LocalDate date;
 
-    public RuleViolation toEntity(Member member){
-        return RuleViolation.builder()
-                .member(member)
-                .rule(rule)
-                .date(date)
-                .build();
-    }
 }
