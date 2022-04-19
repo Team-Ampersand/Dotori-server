3c3a7bf1d7230366be9eaf214219f55867fd3a51
qoxogus <a01034367055@gmail.com>
2022-04-19 11:07:17 +0900

    [UPDATE] s3 인터페이스와 구현체로 분리

--- src/main/java/com/server/Dotori/domain/board/service/S3Service.java
+++ src/main/java/com/server/Dotori/domain/board/service/S3Service.java
@@ -1,57 +1,10 @@
 package com.server.Dotori.domain.board.service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
+public interface S3Service {
 
-@Service
-@RequiredArgsConstructor
-public class S3Service {
+    String uploadFile(MultipartFile multipartFile);
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-    private final AmazonS3 amazonS3;
-
-    public String uploadFile(MultipartFile multipartFile) {
-        String fileName = createFileName(multipartFile.getOriginalFilename());
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentLength(multipartFile.getSize());
-        objectMetadata.setContentType(multipartFile.getContentType());
-
-        try(InputStream inputStream = multipartFile.getInputStream()) {
-            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead));
-
-        } catch(IOException e) {
-            throw new IllegalStateException("파일 업로드에 실패했습니다.");
-        }
-        return fileName;
-    }
-
-    public void deleteFile(String fileName) {
-        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
-    }
-
-    private String createFileName(String fileName) {
-        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
-    }
-
-    private String getFileExtension(String fileName) {
-        try {
-            return fileName.substring(fileName.lastIndexOf("."));
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("잘못된 형식의 파일입니다.");
-        }
-    }
+    void deleteFile(String fileName);
 }
