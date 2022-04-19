a53ba45e514e3ea84fa4a53ba8a18de210b68b68
taem1n1 <youman7953@naver.com>
2022-04-19 09:51:43 +0900

    주석 수정

--- src/main/java/com/server/Dotori/domain/massage/service/Impl/MassageServiceImpl.java
+++ src/main/java/com/server/Dotori/domain/massage/service/Impl/MassageServiceImpl.java
@@ -44,7 +44,6 @@ public class MassageServiceImpl implements MassageService {
      * 5명이 신청가능, 안마의자 신청 상태가 'CAN'일때만 신청가능, 금토일은 신청 불가능
      * 주중 금토일을 제외한 20시 20분 ~ 21시 사이에 신청가능
      * 안마의자 신청시 상태가 'CAN'에서 'APPLIED'로 변경
-     * 안마의자 신청 만료기간을 현재기준으로 +1달을 추가해서 ExpiredTime에 저장
      * @param dayOfWeek 현재 요일
      * @param hour 현재 시
      * @param min 현재 분
@@ -85,7 +84,6 @@ public class MassageServiceImpl implements MassageService {
      * 주중 금토일을 제외한 20시 20분 ~ 21시 사이에 취소가능
      * 안마의자 신청 취소시 상태가 "APPLIED"에서 "CANT"로 변경
      * 안마의자 신청을 취소한 학생은 MASSAGE 테이블에서 삭제
-     * 안마의자 신청 만료기간을 null로 변경
      * @param dayOfWeek 현재 요일
      * @param hour 현재 시
      * @param min 현재 분
@@ -111,9 +109,7 @@ public class MassageServiceImpl implements MassageService {
     }
 
     /**
-     * updateUnBanMassage 마지막 안마의자 신청일이 한달지난 학생의 안마의자 신청 상태를 'IMPOSSIBLE'에서 'CAN'으로 변경
      * updateMassageStatusCant 안마의자 신청을 했다가 취소를 한 학생의 상태를 'CAN'으로 변경
-     * updateMassageStatusImpossible 안마의자 신청을 한 학생의 상태를 'IMPOSSIBLE'로 변경
      * deleteAll 안마의자를 신청한 학생 명단 테이블을 초기화
      * 해당 쿼리들은 주중 금,토,일을 제외한 새벽 2시에 Scheduler로 동작
      * @author 김태민
@@ -126,8 +122,8 @@ public class MassageServiceImpl implements MassageService {
     }
 
     /**
-     * 안마의자 신청을 한 학생수와 현재 자신의 안마의자 신청 상태와 안마의자 신청 금지 만료일을 조회하는 로직
-     * @return Map <String massageStatus(안마의자 신청 상태), String count(안마의자 신청 카운트), String expiredTime(안마의자 신청 금지 만료일)>
+     * 안마의자 신청을 한 학생수와 현재 자신의 안마의자 신청 상태를 조회하는 로직
+     * @return Map <String massageStatus(안마의자 신청 상태), String count(안마의자 신청 카운트)>
      * @author 김태민
      */
     @Override
