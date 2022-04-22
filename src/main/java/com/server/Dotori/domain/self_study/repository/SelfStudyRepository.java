956700cc73548e2d19875dfc0fc3909111881a4a
qoxogus <a01034367055@gmail.com>
2022-04-22 16:04:02 +0900

    [FIX] 멀티스레드 환경에서 동시성 이슈 비관적 Lock을 이용하여 해결

--- src/main/java/com/server/Dotori/domain/self_study/repository/SelfStudyRepository.java
+++ src/main/java/com/server/Dotori/domain/self_study/repository/SelfStudyRepository.java
@@ -5,12 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
-import static javax.persistence.LockModeType.PESSIMISTIC_READ;
+import javax.persistence.LockModeType;
 
 @Repository
 public interface SelfStudyRepository extends JpaRepository<SelfStudy, Long>, SelfStudyRepositoryCustom {
     void deleteByMemberId(Long id);
 
-    @Lock(PESSIMISTIC_READ)
+    @Lock(LockModeType.PESSIMISTIC_READ)
     long count();
 }
