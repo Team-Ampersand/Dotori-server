8d5b1ffc2b3f3f1527fd1baded4bb7efd59b2196
qoxogus <a01034367055@gmail.com>
2022-05-16 14:23:14 +0900

    [UPDATE] MemberRepository에서 관리하던 selfStudy 관련 쿼리 이동

--- src/main/java/com/server/Dotori/domain/member/repository/member/MemberRepositoryCustom.java
+++ src/main/java/com/server/Dotori/domain/member/repository/member/MemberRepositoryCustom.java
@@ -4,16 +4,11 @@ import com.server.Dotori.domain.main_page.dto.GetProfileDto;
 import com.server.Dotori.domain.massage.dto.MassageStudentsDto;
 import com.server.Dotori.domain.member.Member;
 import com.server.Dotori.domain.point.dto.GetAboutPointDto;
-import com.server.Dotori.domain.self_study.dto.SelfStudyStudentsDto;
 import com.server.Dotori.domain.rule.dto.FindStusDto;
 
 import java.util.List;
 
 public interface MemberRepositoryCustom {
-
-    List<SelfStudyStudentsDto> findBySelfStudyAPLLIED();
-
-    List<SelfStudyStudentsDto> findBySelfStudyCategory(Long id);
 
     void updateSelfStudyStatus();
 
