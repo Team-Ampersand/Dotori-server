0acae89a6be08e375cee6317594afb0af8f97a57
taem1n1 <youman7953@naver.com>
2022-05-18 15:50:21 +0900

    Lock을 이용해 for update 문으로 MassageCount 엔티티 가져오기

--- src/main/java/com/server/Dotori/domain/massage/repository/MassageCountRepository.java
+++ src/main/java/com/server/Dotori/domain/massage/repository/MassageCountRepository.java
@@ -1,2 +1,13 @@
-package com.server.Dotori.domain.massage.repository;public class MassageCountRepository {
+package com.server.Dotori.domain.massage.repository;
+
+import com.server.Dotori.domain.massage.MassageCount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import javax.persistence.LockModeType;
+
+public interface MassageCountRepository extends JpaRepository<MassageCount, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    MassageCount findMassageCountById(Long id);
 }
