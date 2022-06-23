550f38b2fcf9b2607ecfe314d685759482667fe3
jeayoung <k01066624566@gmail.com>
2022-06-23 13:59:30 +0900

    안쓰는 임포트 제거

--- src/main/java/com/server/Dotori/domain/member/Member.java
+++ src/main/java/com/server/Dotori/domain/member/Member.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
