0fb0b1c429f043b4b038ad47f96a4817b83e91f7
Huuuunee <s21024@gsm.hs.kr>
2022-08-05 15:52:39 +0900

    add BoardImageRepository

--- src/main/java/com/server/Dotori/domain/board/repository/BoardImageRepository.java
+++ src/main/java/com/server/Dotori/domain/board/repository/BoardImageRepository.java
@@ -1,2 +1,8 @@
-package com.server.Dotori.domain.board.repository;public interface BoardImageRepository {
+package com.server.Dotori.domain.board.repository;
+
+import com.server.Dotori.domain.board.BoardImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardImageRepository extends JpaRepository<BoardImage,Long> {
+    String findBoardImageByBoardId(Long boardId);
 }
