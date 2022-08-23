631ae3a22cbc22cc44cb0e0fb6ae0cd03e46330a
Huuuunee <s21024@gsm.hs.kr>
2022-08-23 10:49:47 +0900

    feat 삭제 jpa작성

--- src/main/java/com/server/Dotori/domain/board/repository/BoardImageRepository.java
+++ src/main/java/com/server/Dotori/domain/board/repository/BoardImageRepository.java
@@ -3,6 +3,9 @@ package com.server.Dotori.domain.board.repository;
 import com.server.Dotori.domain.board.BoardImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardImageRepository extends JpaRepository<BoardImage,Long> {
-    String findBoardImageByBoardId(Long boardId);
+    List<BoardImage> getBoardImageByBoardId(Long boardId);
+    void deleteByBoardId(Long boardId);
 }
